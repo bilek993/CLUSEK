78908078c6018d2315cab1f39b67453b31fcfe24
LAPTOP-CCQ1KV34\jbili <jbilinskipl@gmail.com>
2019-06-26 19:55:56 +0200

    Added comment about entry point.

--- Engine/src/Source.cpp
+++ Engine/src/Source.cpp
@@ -4,6 +4,7 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "DirectXTK.lib")
 
+// Entry point for the Engine
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance, 
 	_In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
