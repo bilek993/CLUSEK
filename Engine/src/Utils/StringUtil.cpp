5110719a6f614ad8adedc0b9f9ab943548eecd16
bilek993 <jbilinskipl@gmail.com>
2019-07-18 21:25:53 +0200

    Added finding extenstion of file.

--- Engine/src/Utils/StringUtil.cpp
+++ Engine/src/Utils/StringUtil.cpp
@@ -5,3 +5,14 @@ std::wstring StringUtil::StringToWide(const std::string& input)
 	std::wstring outputWideString(input.begin(), input.end());
 	return outputWideString;
 }
+
+std::string StringUtil::FindExtension(std::string input)
+{
+	const auto position = input.find_last_of('.');
+
+	if (position == std::string::npos)
+		return "";
+
+	std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+	return input.substr(position + 1);
+}
--- Engine/src/Utils/StringUtil.h
+++ Engine/src/Utils/StringUtil.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <string>
+#include <algorithm>
 
 class StringUtil final
 {
 public:
 	static std::wstring StringToWide(const std::string& input);
+	static std::string FindExtension(std::string input);
 };
