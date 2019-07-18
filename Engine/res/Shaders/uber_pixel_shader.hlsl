c8ab3963472ac5a140e090ebf2c41f119565b734
bilek993 <jbilinskipl@gmail.com>
2019-07-18 20:45:43 +0200

    Renamed pixel shader texture to match name of c++ code variable.

--- Engine/res/Shaders/uber_pixel_shader.hlsl
+++ Engine/res/Shaders/uber_pixel_shader.hlsl
@@ -4,11 +4,11 @@ struct PS_INPUT
     float2 TextureCoord : TEXCOORD;
 };
 
-Texture2D Texture : TEXTURE : register(t0);
+Texture2D MainTexture : TEXTURE : register(t0);
 SamplerState Sampler : SAMPLER : register(s0);
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
-    float3 pixelColor = Texture.Sample(Sampler, input.TextureCoord);
+    float3 pixelColor = MainTexture.Sample(Sampler, input.TextureCoord);
     return float4(pixelColor, 1.0f);
 }
