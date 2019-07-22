de22d05df57f7c04271b1a7fa1c62e9ca7f8539b
bilek993 <jbilinskipl@gmail.com>
2019-07-22 21:18:35 +0200

    Moved fps counting logic to new function;

--- Engine/src/GUI/Windows/FpsTimerWindow.cpp
+++ Engine/src/GUI/Windows/FpsTimerWindow.cpp
@@ -2,18 +2,22 @@
 #include <imgui.h>
 void FpsTimerWindow::Draw(const float deltaTime)
 {
+	Calculate(deltaTime);
+
+	ImGui::Begin("FPS Timer", &IsEnabled);
+	ImGui::Text("Frames per second: %i", ShowedFps);
+	ImGui::End();
+}
+
+void FpsTimerWindow::Calculate(const float deltaTime)
+{
 	FramesCounter += 1;
 	TimeCounter += deltaTime;
 
 	if (TimeCounter > 1000.0f)
 	{
 		ShowedFps = FramesCounter;
-
 		FramesCounter = 0;
 		TimeCounter = 0.0f;
 	}
-
-	ImGui::Begin("FPS Timer", &IsEnabled);
-	ImGui::Text("Frames per second: %i", ShowedFps);
-	ImGui::End();
 }
--- Engine/src/GUI/Windows/FpsTimerWindow.h
+++ Engine/src/GUI/Windows/FpsTimerWindow.h
@@ -5,6 +5,7 @@ class FpsTimerWindow final
 public:
 	inline static bool IsEnabled = false;
 	static void Draw(float deltaTime);
+	static void Calculate(float deltaTime);
 private:
 	inline static int FramesCounter = 0;
 	inline static float TimeCounter = 0.0f;
