5c9e1fe64eb8ffe3a789f16067990875c0669510
bilek993 <jbilinskipl@gmail.com>
2019-07-25 20:55:37 +0200

    Fine tuned color values.

--- Engine/src/Renderer/LightSettings.h
+++ Engine/src/Renderer/LightSettings.h
@@ -3,10 +3,10 @@
 
 struct LightSettings final
 {
-	DirectX::XMFLOAT3 AmbientLightColor = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
-	float AmbientLightStrength = 1.0f;
+	DirectX::XMFLOAT3 AmbientLightColor = DirectX::XMFLOAT3(0.6f, 0.8f, 1.0f);
+	float AmbientLightStrength = 0.5f;
 
-	DirectX::XMFLOAT3 DirectionalLightColor = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
-	float DirectionalLightStrength = 1.0f;
+	DirectX::XMFLOAT3 DirectionalLightColor = DirectX::XMFLOAT3(0.9f, 0.9f, 1.0f);
+	float DirectionalLightStrength = 0.75f;
 	DirectX::XMFLOAT3 DirectionalLightDirection = DirectX::XMFLOAT3(0.25f, 0.5f, -1.0f);
 };
