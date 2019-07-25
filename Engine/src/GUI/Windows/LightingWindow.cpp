68511a9d0fa1d854641cb4aa7f143d51ebdef56a
bilek993 <jbilinskipl@gmail.com>
2019-07-25 21:29:01 +0200

    Added directional lighting settings in debug gui.

--- Engine/src/GUI/Windows/LightingWindow.cpp
+++ Engine/src/GUI/Windows/LightingWindow.cpp
@@ -5,11 +5,20 @@ void LightingWindow::Draw(LightSettings& lightSettings)
 {
 	ImGui::Begin("Lighting settings");
 	ImGui::Text("Ambient light settings:");
-	ImGui::ColorPicker3("Color", 
+	ImGui::ColorPicker3("Color##Ambient", 
 						&lightSettings.AmbientLightColor.x,
 					ImGuiColorEditFlags_NoAlpha | 
 						ImGuiColorEditFlags_PickerHueBar | 
 						ImGuiColorEditFlags_NoInputs);
-	ImGui::DragFloat("Strength", &lightSettings.AmbientLightStrength, 0.001f, 0.0f, 1.0f);
+	ImGui::DragFloat("Strength##Ambient", &lightSettings.AmbientLightStrength, 0.001f, 0.0f, 1.0f);
+	ImGui::Separator();
+	ImGui::Text("Directional light settings:");
+	ImGui::ColorPicker3("Color##Directional",
+						&lightSettings.DirectionalLightColor.x,
+						ImGuiColorEditFlags_NoAlpha |
+						ImGuiColorEditFlags_PickerHueBar |
+						ImGuiColorEditFlags_NoInputs);
+	ImGui::DragFloat("Strength##Directional", &lightSettings.DirectionalLightStrength, 0.001f, 0.0f, 1.0f);
+	ImGui::DragFloat3("Direction##Directional", &lightSettings.DirectionalLightDirection.x, 0.001f, -1.0f, 1.0f);
 	ImGui::End();
 }
