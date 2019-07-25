0fb9dd7ab2cf577fe1bd2fa63c9a8de4a6eb09d3
bilek993 <jbilinskipl@gmail.com>
2019-07-25 20:55:12 +0200

    Changed default color settings and moved directional light values into struct.

--- Engine/src/Renderer/LightSettings.h
+++ Engine/src/Renderer/LightSettings.h
@@ -5,4 +5,8 @@ struct LightSettings final
 {
 	DirectX::XMFLOAT3 AmbientLightColor = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
 	float AmbientLightStrength = 1.0f;
+
+	DirectX::XMFLOAT3 DirectionalLightColor = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
+	float DirectionalLightStrength = 1.0f;
+	DirectX::XMFLOAT3 DirectionalLightDirection = DirectX::XMFLOAT3(0.25f, 0.5f, -1.0f);
 };
