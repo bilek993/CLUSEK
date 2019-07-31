9b262178992739dfc874c96726a0dca1d39420a3
bilek993 <jbilinskipl@gmail.com>
2019-07-31 21:59:37 +0200

    Fixed units from m/s to ms.

--- Engine/src/GUI/Windows/FpsTimerWindow.cpp
+++ Engine/src/GUI/Windows/FpsTimerWindow.cpp
@@ -11,9 +11,9 @@ void FpsTimerWindow::Draw()
 	const auto minFps = std::min_element(std::begin(HistoricalFps), std::end(HistoricalFps));
 
 	ImGui::Begin("FPS Timer", &IsEnabled);
-	ImGui::Text("Current: %.0f fps (%.2f m/s)", currentFps, 1000.0f / currentFps);
-	ImGui::Text("Minimum: %.0f fps (%.2f m/s)", *minFps, 1000.0f / *minFps);
-	ImGui::Text("Maximum: %.0f fps (%.2f m/s)", *maxFps, 1000.0f / *maxFps);
+	ImGui::Text("Current: %.0f fps (%.2f ms)", currentFps, 1000.0f / currentFps);
+	ImGui::Text("Minimum: %.0f fps (%.2f ms)", *minFps, 1000.0f / *minFps);
+	ImGui::Text("Maximum: %.0f fps (%.2f ms)", *maxFps, 1000.0f / *maxFps);
 	ImGui::Separator();
 	ImGui::PlotLines("", HistoricalFps, IM_ARRAYSIZE(HistoricalFps), 0, nullptr, FLT_MAX, FLT_MAX, ImVec2(200, 50));
 	ImGui::End();
