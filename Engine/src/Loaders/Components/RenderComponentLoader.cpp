98742ee6f1c0d6fa241e3376644615e061ada582
bilek993 <jbilinskipl@gmail.com>
2019-07-31 18:10:08 +0200

    Added getting parameters for render component.

--- Engine/src/Loaders/Components/RenderComponentLoader.cpp
+++ Engine/src/Loaders/Components/RenderComponentLoader.cpp
@@ -4,5 +4,10 @@
 void RenderComponentLoader::Add(const nlohmann::json& json, entt::registry& registry,
 	const entt::registry::entity_type& entity)
 {
-	registry.assign<RenderComponent>(entity, "Nanosuit", "Data/Models/Nanosuit/nanosuit-material.json");
+	auto &component = registry.assign<RenderComponent>(entity);
+
+	if (!json["ModelId"].is_null())
+		component.ModelId = json["ModelId"].get<std::string>();
+	if (!json["MaterialId"].is_null())
+		component.MaterialId = json["MaterialId"].get<std::string>();
 }
