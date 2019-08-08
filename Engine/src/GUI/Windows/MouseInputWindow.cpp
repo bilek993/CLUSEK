50be687afd2cfe6e4820100ba961c35e519471fa
bilek993 <jbilinskipl@gmail.com>
2019-08-08 21:39:00 +0200

    Added functionality for mouse input window.

--- Engine/src/GUI/Windows/MouseInputWindow.cpp
+++ Engine/src/GUI/Windows/MouseInputWindow.cpp
@@ -4,5 +4,19 @@
 void MouseInputWindow::Draw()
 {
 	ImGui::Begin("Mouse input", &IsEnabled);
+
+	ImGui::Text("Left mouse button: %s", IoData->MouseState.leftButton ? "X" : " ");
+	ImGui::Text("Middle mouse button: %s", IoData->MouseState.middleButton ? "X" : " ");
+	ImGui::Text("Right mouse button: %s", IoData->MouseState.rightButton ? "X" : " ");
+
+	ImGui::Separator();
+
+	ImGui::Text("Mouse delta x: %d", IoData->MouseState.x);
+	ImGui::Text("Mouse delta y: %d", IoData->MouseState.y);
+
+	ImGui::Separator();
+
+	ImGui::Text("Scroll wheel position: %d", IoData->MouseState.scrollWheelValue);
+
 	ImGui::End();
 }
