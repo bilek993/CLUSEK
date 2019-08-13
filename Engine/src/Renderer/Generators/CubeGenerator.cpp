e5b498c62bba0eb2b2c0e696f428d8ccc0435afb
bilek993 <jbilinskipl@gmail.com>
2019-08-13 18:06:46 +0200

    Inverted skybox faces.

--- Engine/src/Renderer/Generators/CubeGenerator.cpp
+++ Engine/src/Renderer/Generators/CubeGenerator.cpp
@@ -24,18 +24,18 @@ void CubeGenerator::Generate(ID3D11Device* device, VertexBuffer<PositionVertex>&
 
 	DWORD indices[] =
 	{
-		0, 1, 2,
-		0, 2, 3,
-		4, 7, 6, 
-		4, 6, 5,
-		3, 2, 6,
-		3, 6, 7,
-		4, 5, 1,
-		4, 1, 0,
-		1, 5, 6,
-		1, 6, 2,
-		0, 3, 7,
-		0, 7, 4,
+		2, 1, 0,
+		3, 2, 0,
+		6, 7, 4, 
+		5, 6, 4,
+		6, 2, 3,
+		7, 6, 3,
+		1, 5, 4,
+		0, 1, 4,
+		6, 5, 1,
+		2, 6, 1,
+		7, 3, 0,
+		4, 7, 0,
 	};
 
 	Logger::Debug("Preparing to generate indices buffer...");
