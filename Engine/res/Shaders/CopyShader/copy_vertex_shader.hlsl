9a8c81ae920647f0ae64e5f6228707d5b02db494
bilek993 <jbilinskipl@gmail.com>
2019-08-23 10:15:08 +0200

    Fixed post processing texture coordinates.

--- Engine/res/Shaders/CopyShader/copy_vertex_shader.hlsl
+++ Engine/res/Shaders/CopyShader/copy_vertex_shader.hlsl
@@ -13,7 +13,8 @@ VS_OUTPUT main(VS_INPUT input)
 {
     VS_OUTPUT output;
     output.Position = float4(input.Position, 1.0f);
-    output.TextureCoord = input.Position.xy;
+    output.TextureCoord = (input.Position.xy + 1) / 2;
+    output.TextureCoord.y *= -1;
 
     return output;
 }
