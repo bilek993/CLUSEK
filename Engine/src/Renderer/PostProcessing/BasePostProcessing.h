6f56db58f0ba8036f982fb6dd9f83866ccccb0f4
bilek993 <jbilinskipl@gmail.com>
2019-08-23 10:45:24 +0200

    Added more info in comment about hotfix.

--- Engine/src/Renderer/PostProcessing/BasePostProcessing.h
+++ Engine/src/Renderer/PostProcessing/BasePostProcessing.h
@@ -30,7 +30,7 @@ void BasePostProcessing::Draw(const VertexBuffer<T>& vertexBuffer, const IndexBu
 	DeviceContext->IASetIndexBuffer(indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
 	DeviceContext->DrawIndexed(indexBuffer.GetIndexCount(), 0, 0);
 
-	// Fix for hazard problems
+	// Fix for hazard problems (https://gamedev.stackexchange.com/questions/95591/directx-rendertargettexture-bound-to-both-rtv-and-srv)
 	ID3D11ShaderResourceView* null[] = { nullptr, nullptr, nullptr };
 	DeviceContext->PSSetShaderResources(0, 3, null);
 }
