c79041228964ffc978dd354ba98b304917d4c059
bilek993 <jbilinskipl@gmail.com>
2019-08-30 19:56:23 +0200

    Post processing effects can be removed from stack.

--- Engine/src/GUI/Windows/PostProcessingWindow.cpp
+++ Engine/src/GUI/Windows/PostProcessingWindow.cpp
@@ -4,12 +4,18 @@
 void PostProcessingWindow::Draw()
 {
 	ImGui::Begin("Post Processing settings", &IsEnabled);
-
 	ImGui::Text("Post processing stack:");
 
-	for (auto& effect : CurrentPostProcessingSettings->List)
+	for (auto i = 0; i < CurrentPostProcessingSettings->List.size(); i++)
 	{
-		ImGui::Text(effect->GetName().c_str());
+		auto buttonLabel = "Remove##PostProcessing_" + std::to_string(i);
+		if (ImGui::SmallButton(buttonLabel.c_str()))
+		{
+			CurrentPostProcessingSettings->List.erase(CurrentPostProcessingSettings->List.begin() + i);
+			break;
+		}
+		ImGui::SameLine();
+		ImGui::Text(CurrentPostProcessingSettings->List[i]->GetName().c_str());
 	}
 
 	ImGui::End();
