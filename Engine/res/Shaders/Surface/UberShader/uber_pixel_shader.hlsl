26624feaca0304b5f42b077c30b80ab099d4717b
bilek993 <jbilinskipl@gmail.com>
2019-09-10 21:45:09 +0200

    Added loading required colors from textues.

--- Engine/res/Shaders/Surface/UberShader/uber_pixel_shader.hlsl
+++ Engine/res/Shaders/Surface/UberShader/uber_pixel_shader.hlsl
@@ -24,14 +24,17 @@ SamplerState Sampler : SAMPLER : register(s0);
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
-    float3 samplerColor = AlbedoTexture.Sample(Sampler, input.TextureCoord).rgb;
-    samplerColor = gammaCorrectTexture(samplerColor);
+    float3 albedoColor = AlbedoTexture.Sample(Sampler, input.TextureCoord).rgb;
+    float3 normalColor = NormalTexture.Sample(Sampler, input.TextureCoord).rgb;
+    float metalicColor = MetalicSmoothnessTexture.Sample(Sampler, input.TextureCoord).r;
+    float smoothnessColor = MetalicSmoothnessTexture.Sample(Sampler, input.TextureCoord).a;
+    float occlusionColor = MetalicSmoothnessTexture.Sample(Sampler, input.TextureCoord).r;
 
-    float3 normalMap = NormalTexture.Sample(Sampler, input.TextureCoord).rgb;
+    albedoColor = gammaCorrectTexture(albedoColor);
 
-    float3 directionalLight = saturate(dot(DirectionalLightDirection, calculateNormal(normalMap, input.TBN)) * (DirectionalLightColor * DirectionalLightStrength) * samplerColor);
+    float3 directionalLight = saturate(dot(DirectionalLightDirection, calculateNormal(normalColor, input.TBN)) * (DirectionalLightColor * DirectionalLightStrength) * albedoColor);
 
-    float3 finalColor = samplerColor + directionalLight;
+    float3 finalColor = albedoColor + directionalLight;
 
     return float4(finalColor, Alpha);
 }
