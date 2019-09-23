fb9db04cf5389a250c2efcff7cca989425d5cf4c
bilek993 <jbilinskipl@gmail.com>
2019-09-23 19:14:44 +0200

    Multisampling count can be changed without any problems.

--- Engine/res/Shaders/PostProcessing/multisampling_pixel_shader.hlsl
+++ Engine/res/Shaders/PostProcessing/multisampling_pixel_shader.hlsl
@@ -4,17 +4,20 @@ struct PS_INPUT
     float2 TextureCoord : TEXCOORD;
 };
 
-Texture2DMS<float4, 4> InputTexture : register(t0);
+Texture2DMS<float4> InputTexture : register(t0);
 SamplerState Sampler : register(s0);
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
+    float inputMipLevel, inputHeight, numberOfSamples;
+    InputTexture.GetDimensions(inputMipLevel, inputHeight, numberOfSamples);
+
     float4 colorSum = float4(0.0f, 0.0f, 0.0f, 0.0f);
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < numberOfSamples; i++)
     {
         colorSum += InputTexture.Load(input.Position.xy, i);
     }
 
-    return colorSum / 4;
+    return colorSum / numberOfSamples;
 }
