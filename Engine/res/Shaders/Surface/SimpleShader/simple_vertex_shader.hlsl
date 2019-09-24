6810f57d7956c0259325bef488dd1dbbe9c19c03
bilek993 <jbilinskipl@gmail.com>
2019-09-24 21:49:07 +0200

    Small changes in shader.

--- Engine/res/Shaders/Surface/SimpleShader/simple_vertex_shader.hlsl
+++ Engine/res/Shaders/Surface/SimpleShader/simple_vertex_shader.hlsl
@@ -18,7 +18,7 @@ VS_OUTPUT main(VS_INPUT input)
 {
     VS_OUTPUT output;
     output.Position = mul(float4(input.Position, 1.0f), WorldViewProjectionMat).xyww;
-    output.TextureCoord = (input.Position.xy + 0.5f);
+    output.TextureCoord = input.Position.xy + 0.5f;
     output.TextureCoord.y *= -1;
 
     return output;
