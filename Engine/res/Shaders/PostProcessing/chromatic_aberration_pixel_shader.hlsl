8cd47586875f12407bd8cd3c60afffd5bfae453b
bilek993 <jbilinskipl@gmail.com>
2019-09-26 20:11:17 +0200

    Added basic chromatic aberration effect.

--- Engine/res/Shaders/PostProcessing/chromatic_aberration_pixel_shader.hlsl
+++ Engine/res/Shaders/PostProcessing/chromatic_aberration_pixel_shader.hlsl
@@ -7,8 +7,14 @@ struct PS_INPUT
 Texture2D InputTexture : register(t0);
 SamplerState Sampler : register(s1);
 
+static const float SIZE = 0.00025f;
+
 float4 main(PS_INPUT input) : SV_TARGET
 {
     float3 output = InputTexture.Sample(Sampler, input.TextureCoord).rgb;
+
+    output.r = InputTexture.Sample(Sampler, input.TextureCoord + float2(SIZE, 0.0f)).r;
+    output.b = InputTexture.Sample(Sampler, input.TextureCoord - float2(SIZE, 0.0f)).b;
+
     return float4(output, 1.0f);
 }
