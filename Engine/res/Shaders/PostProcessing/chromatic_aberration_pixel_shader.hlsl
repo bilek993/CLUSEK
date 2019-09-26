86b128c41e4012ee71771671b252f71494d89b7b
bilek993 <jbilinskipl@gmail.com>
2019-09-26 21:11:27 +0200

    Added chromatic aberration intensity based on calculation.

--- Engine/res/Shaders/PostProcessing/chromatic_aberration_pixel_shader.hlsl
+++ Engine/res/Shaders/PostProcessing/chromatic_aberration_pixel_shader.hlsl
@@ -7,14 +7,28 @@ struct PS_INPUT
 Texture2D InputTexture : register(t0);
 SamplerState Sampler : register(s1);
 
-static const float SIZE = 0.00025f;
+static const float2 CENTER = float2(0.5f, 0.5f);
+static const float AVOID_DISTANCE = 0.2f;
+static const float SIZE = 0.075f;
+static const float ATTENUATION = 5.0f;
+
+float calculateIntensity(float2 textureCoord)
+{
+    float offset = distance(CENTER, textureCoord) - AVOID_DISTANCE;
+    offset = saturate(offset);
+    offset = pow(offset, ATTENUATION);
+
+    return offset;
+}
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
     float3 output = InputTexture.Sample(Sampler, input.TextureCoord).rgb;
 
-    output.r = InputTexture.Sample(Sampler, input.TextureCoord + float2(SIZE, 0.0f)).r;
-    output.b = InputTexture.Sample(Sampler, input.TextureCoord - float2(SIZE, 0.0f)).b;
+    float intensity = calculateIntensity(input.TextureCoord);
+
+    output.r = InputTexture.Sample(Sampler, input.TextureCoord + float2(SIZE * intensity, 0.0f)).r;
+    output.b = InputTexture.Sample(Sampler, input.TextureCoord - float2(SIZE * intensity, 0.0f)).b;
 
     return float4(output, 1.0f);
 }
