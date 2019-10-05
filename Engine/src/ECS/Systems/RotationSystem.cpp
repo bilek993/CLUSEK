e8fa2023d924e2d529eaa83a8ebb3dac7e2a2fc2
bilek993 <jbilinskipl@gmail.com>
2019-10-05 18:00:27 +0200

    Removed initialization in rotation system.

--- Engine/src/ECS/Systems/RotationSystem.cpp
+++ Engine/src/ECS/Systems/RotationSystem.cpp
@@ -6,11 +6,6 @@
 
 void RotationSystem::Start()
 {
-	Logger::Debug("Staring rotation system...");
-	Registry->view<TransformComponent, ModelRenderComponent>().each([](TransformComponent &transformComponent, ModelRenderComponent &modelRenderComponent)
-	{
-		ModelViewLogic::UpdateModelMatrix(modelRenderComponent.WorldMatrix, transformComponent);
-	});
 }
 
 void RotationSystem::Update(const float deltaTime)
@@ -18,6 +13,5 @@ void RotationSystem::Update(const float deltaTime)
 	Registry->view<TransformComponent, ModelRenderComponent>().each([&deltaTime](TransformComponent &transformComponent, ModelRenderComponent &modelRenderComponent)
 	{
 		TransformLogic::AdjustRotation(0.0f, 0.001f * deltaTime, 0.0f, transformComponent);
-		ModelViewLogic::UpdateModelMatrix(modelRenderComponent.WorldMatrix, transformComponent);
 	});
 }
