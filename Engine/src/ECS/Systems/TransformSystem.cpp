86cd6c38067fc304d50a18c079bec27f323ce64c
bilek993 <jbilinskipl@gmail.com>
2019-10-05 18:46:36 +0200

    Made recalculation function generic.

--- Engine/src/ECS/Systems/TransformSystem.cpp
+++ Engine/src/ECS/Systems/TransformSystem.cpp
@@ -1,35 +1,18 @@
 #include "TransformSystem.h"
-#include "../Components/TransformComponent.h"
-#include "../Components/ModelRenderComponent.h"
-#include "../../Renderer/ModelViewLogic.h"
-#include "../Components/SkyboxComponent.h"
 
 void TransformSystem::Start()
 {
+	Logger::Debug("Staring transform system...");
+	RecalculateAll();
 }
 
 void TransformSystem::Update(float deltaTime)
 {
-	Recalculate();
+	RecalculateAll();
 }
 
-void TransformSystem::Recalculate() const
+void TransformSystem::RecalculateAll() const
 {
-	Registry->view<TransformComponent, ModelRenderComponent>().each([](TransformComponent &transformComponent, ModelRenderComponent &modelRenderComponent)
-	{
-		if (transformComponent.ValuesChanged)
-		{
-			transformComponent.ValuesChanged = false;
-			ModelViewLogic::UpdateModelMatrix(modelRenderComponent.WorldMatrix, transformComponent);
-		}
-	});
-
-	Registry->view<TransformComponent, SkyboxComponent>().each([](TransformComponent &transformComponent, SkyboxComponent &skyboxComponent)
-	{
-		if (transformComponent.ValuesChanged)
-		{
-			transformComponent.ValuesChanged = false;
-			ModelViewLogic::UpdateModelMatrix(skyboxComponent.WorldMatrix, transformComponent);
-		}
-	});
+	Recalculate<ModelRenderComponent>();
+	Recalculate<SkyboxComponent>();
 }
--- Engine/src/ECS/Systems/TransformSystem.h
+++ Engine/src/ECS/Systems/TransformSystem.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "BaseSystem.h"
+#include "../Components/TransformComponent.h"
+#include "../Components/ModelRenderComponent.h"
+#include "../../Renderer/ModelViewLogic.h"
+#include "../Components/SkyboxComponent.h"
+
 
 class TransformSystem final : public BaseSystem
 {
@@ -7,5 +12,21 @@ public:
 	void Start() override;
 	void Update(float deltaTime) override;
 private:
+	void RecalculateAll() const;
+
+	template<class T>
 	void Recalculate() const;
 };
+
+template <class T>
+void TransformSystem::Recalculate() const
+{
+	Registry->view<TransformComponent, T>().each([](TransformComponent &transformComponent, T &secondComponent)
+	{
+		if (transformComponent.ValuesChanged)
+		{
+			transformComponent.ValuesChanged = false;
+			ModelViewLogic::UpdateModelMatrix(secondComponent.WorldMatrix, transformComponent);
+		}
+	});
+}
