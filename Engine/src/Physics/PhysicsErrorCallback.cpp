9ca49c2f441e6d450b87ba502312bae740e48301
bilek993 <jbilinskipl@gmail.com>
2019-10-27 18:38:03 +0100

    Changed error handling in PhysX callback.

--- Engine/src/Physics/PhysicsErrorCallback.cpp
+++ Engine/src/Physics/PhysicsErrorCallback.cpp
@@ -3,9 +3,9 @@
 
 void PhysicsErrorCallback::reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, const int line)
 {
-	Logger::Error("PhysX reported an error! Engine may work unstable after this error!");
 	Logger::Error(
-		"Line: " + std::to_string(line) + "\n" +
-		"File: " + std::string(file) + "\n" +
+		"PhysX reported an error! Line: " + 
+		std::to_string(line) + " " +
+		"File: " + std::string(file) + " " +
 		"Message: " + std::string(message));
 }
