65f798a2b44c84ecb798e93cf8db09fe4abd82bf
bilek993 <jbilinskipl@gmail.com>
2019-11-04 19:40:26 +0100

    Added support for handbrake in vehicles.

--- Engine/src/ECS/Systems/VehiclePlayerControllerSystem.cpp
+++ Engine/src/ECS/Systems/VehiclePlayerControllerSystem.cpp
@@ -9,14 +9,14 @@ void VehiclePlayerControllerSystem::Start()
 
 void VehiclePlayerControllerSystem::Update(const float deltaTime)
 {
-	float accelerate, brake, left;
+	float accelerate, brake, left, handbrake;
 
 	if (InputOutputData->GamePadState.IsConnected())
-		HandleGamepad(accelerate, brake, left);
+		HandleGamepad(accelerate, brake, left, handbrake);
 	else
-		HandleKeyboard(accelerate, brake, left);
+		HandleKeyboard(accelerate, brake, left, handbrake);
 
-	Registry->view<VehiclePlayerControllerComponent, VehicleComponent>().each([&accelerate, &brake, &left](VehiclePlayerControllerComponent &vehiclePlayerControllerComponent,
+	Registry->view<VehiclePlayerControllerComponent, VehicleComponent>().each([&accelerate, &brake, &left, &handbrake](VehiclePlayerControllerComponent &vehiclePlayerControllerComponent,
 		VehicleComponent &vehicleComponent)
 	{
 		if (vehiclePlayerControllerComponent.Possessed)
@@ -24,20 +24,23 @@ void VehiclePlayerControllerSystem::Update(const float deltaTime)
 			vehicleComponent.Vehicle->mDriveDynData.setAnalogInput(physx::PxVehicleDrive4WControl::eANALOG_INPUT_ACCEL, accelerate);
 			vehicleComponent.Vehicle->mDriveDynData.setAnalogInput(physx::PxVehicleDrive4WControl::eANALOG_INPUT_BRAKE, brake);
 			vehicleComponent.Vehicle->mDriveDynData.setAnalogInput(physx::PxVehicleDrive4WControl::eANALOG_INPUT_STEER_LEFT, left);
+			vehicleComponent.Vehicle->mDriveDynData.setAnalogInput(physx::PxVehicleDrive4WControl::eANALOG_INPUT_HANDBRAKE, handbrake);
 		}
 	});
 }
 
-void VehiclePlayerControllerSystem::HandleGamepad(float& accelerate, float& brake, float& left) const
+void VehiclePlayerControllerSystem::HandleGamepad(float& accelerate, float& brake, float& left, float& handbrake) const
 {
 	accelerate = InputOutputData->GamePadState.triggers.right;
 	brake = InputOutputData->GamePadState.triggers.left;
 	left = -InputOutputData->GamePadState.thumbSticks.leftX;
+	handbrake = InputOutputData->GamePadState.IsAPressed() ? 1.0f : 0.0f;
 }
 
-void VehiclePlayerControllerSystem::HandleKeyboard(float& accelerate, float& brake, float& left) const
+void VehiclePlayerControllerSystem::HandleKeyboard(float& accelerate, float& brake, float& left, float& handbrake) const
 {
 	accelerate = InputOutputData->KeyboardState.Up ? 1.0f : 0.0f;
 	brake = InputOutputData->KeyboardState.Down ? 1.0f : 0.0f;
 	left = InputOutputData->KeyboardState.Left ? 1.0f : InputOutputData->KeyboardState.Right ? -1.0f : 0.0f;
+	handbrake = InputOutputData->KeyboardState.Space ? 1.0f : 0.0f;
 }
