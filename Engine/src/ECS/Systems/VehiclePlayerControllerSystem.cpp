ca44f8dca90fab1b5c2d43d5ffdc041411bf38da
bilek993 <jbilinskipl@gmail.com>
2019-11-04 17:34:28 +0100

    Added VehiclePlayerControllerComponent into main vehicle on map and added controlling vehicle acceleration and brake.

--- Engine/src/ECS/Systems/VehiclePlayerControllerSystem.cpp
+++ Engine/src/ECS/Systems/VehiclePlayerControllerSystem.cpp
@@ -1,11 +1,40 @@
 #include "VehiclePlayerControllerSystem.h"
+#include "../Components/VehicleComponent.h"
+#include "../Components/VehiclePlayerControllerComponent.h"
+#include "PxPhysicsAPI.h"
 
 void VehiclePlayerControllerSystem::Start()
 {
-	// Start
 }
 
-void VehiclePlayerControllerSystem::Update(float deltaTime)
+void VehiclePlayerControllerSystem::Update(const float deltaTime)
 {
-	// Update
+	float accelerate, brake;
+
+	if (InputOutputData->GamePadState.IsConnected())
+		HandleGamepad(accelerate, brake);
+	else
+		HandleKeyboard(accelerate, brake);
+
+	Registry->view<VehiclePlayerControllerComponent, VehicleComponent>().each([&accelerate, &brake](VehiclePlayerControllerComponent &vehiclePlayerControllerComponent,
+		VehicleComponent &vehicleComponent)
+	{
+		if (vehiclePlayerControllerComponent.Possessed)
+		{
+			vehicleComponent.Vehicle->mDriveDynData.setAnalogInput(physx::PxVehicleDrive4WControl::eANALOG_INPUT_ACCEL, accelerate);
+			vehicleComponent.Vehicle->mDriveDynData.setAnalogInput(physx::PxVehicleDrive4WControl::eANALOG_INPUT_BRAKE, brake);
+		}
+	});
+}
+
+void VehiclePlayerControllerSystem::HandleGamepad(float& accelerate, float& brake) const
+{
+	accelerate = InputOutputData->GamePadState.triggers.right;
+	brake = InputOutputData->GamePadState.triggers.left;
+}
+
+void VehiclePlayerControllerSystem::HandleKeyboard(float& accelerate, float& brake) const
+{
+	accelerate = InputOutputData->KeyboardState.Up ? 1.0f : 0.0f;
+	brake = InputOutputData->KeyboardState.Down ? 1.0f : 0.0f;
 }
--- Engine/src/ECS/Systems/VehiclePlayerControllerSystem.h
+++ Engine/src/ECS/Systems/VehiclePlayerControllerSystem.h
@@ -6,4 +6,7 @@ class VehiclePlayerControllerSystem final : public BaseSystem
 public:
 	void Start() override;
 	void Update(float deltaTime) override;
+private:
+	void HandleGamepad(float& accelerate, float& brake) const;
+	void HandleKeyboard(float& accelerate, float& brake) const;
 };
