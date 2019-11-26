56af314f868e7192912447551b9ec8248708045a
bilek993 <jbilinskipl@gmail.com>
2019-11-26 21:16:38 +0100

    Addded support for all components.

--- Engine/src/GUI/Windows/EntityViewerWindow.cpp
+++ Engine/src/GUI/Windows/EntityViewerWindow.cpp
@@ -1,6 +1,19 @@
 #include "EntityViewerWindow.h"
-#include "../../ECS/Components/TransformComponent.h"
 #include "../../ECS/Components/CameraComponent.h"
+#include "../../ECS/Components/ModelRenderComponent.h"
+#include "../../ECS/Components/PhysicsMaterialComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicBoxComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicCapsuleComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicSphereComponent.h"
+#include "../../ECS/Components/RigidbodyStaticBoxComponent.h"
+#include "../../ECS/Components/RigidbodyStaticCapsuleComponent.h"
+#include "../../Loaders/Components/RigidbodyStaticCylinderComponent.h"
+#include "../../ECS/Components/RigidbodyStaticPlaneComponent.h"
+#include "../../ECS/Components/RigidbodyStaticSphereComponent.h"
+#include "../../ECS/Components/SkyboxComponent.h"
+#include "../../ECS/Components/TransformComponent.h"
+#include "../../ECS/Components/VehicleComponent.h"
+#include "../../ECS/Components/VehiclePlayerControllerComponent.h"
 
 void EntityViewerWindow::Draw()
 {
@@ -23,5 +36,20 @@ void EntityViewerWindow::DrawList()
 		ImGui::BulletText("Entity %i", counter++);
 
 		DrawComponent<CameraComponent>(entity, "Camera Component");
+		DrawComponent<ModelRenderComponent>(entity, "Model Render Component");
+		DrawComponent<PhysicsMaterialComponent>(entity, "Physics Material Component");
+		DrawComponent<RigidbodyDynamicBoxComponent>(entity, "Rigidbody Dynamic Box Component");
+		DrawComponent<RigidbodyDynamicCapsuleComponent>(entity, "Rigidbody Dynamic Capsule Component");
+		DrawComponent<RigidbodyDynamicSphereComponent>(entity, "Rigidbody Dynamic Sphere Component");
+		DrawComponent<RigidbodyStaticBoxComponent>(entity, "Rigidbody Static Box Component");
+		DrawComponent<RigidbodyStaticCapsuleComponent>(entity, "Rigidbody Static Capsule Component");
+		DrawComponent<RigidbodyStaticCylinderComponent>(entity, "Rigidbody Static Cylinder Component");
+		DrawComponent<RigidbodyStaticPlaneComponent>(entity, "Rigidbody Static Plane Component");
+		DrawComponent<RigidbodyStaticSphereComponent>(entity, "Rigidbody Static Sphere Component");
+		DrawComponent<SkyboxComponent>(entity, "Skybox Component");
+		DrawComponent<TransformComponent>(entity, "Transform Component");
+		DrawComponent<VehicleComponent>(entity, "Vehicle Component");
+		DrawComponent<VehiclePlayerControllerComponent>(entity, "Vehicle Player Controller Component");
+		DrawComponent<WheelComponent>(entity, "Wheel Component");
 	});
 }
