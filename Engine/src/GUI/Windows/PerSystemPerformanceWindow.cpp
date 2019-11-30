6e555059513ad66e12e3521d91833f63d1bf10ed
bilek993 <jbilinskipl@gmail.com>
2019-11-30 12:42:36 +0100

    Added simple code in window.

--- Engine/src/GUI/Windows/PerSystemPerformanceWindow.cpp
+++ Engine/src/GUI/Windows/PerSystemPerformanceWindow.cpp
@@ -4,5 +4,11 @@
 void PerSystemPerformanceWindow::Draw()
 {
 	ImGui::Begin("Per System Performance Window", &IsEnabled);
+
+	for (auto& system : *Systems)
+	{
+		ImGui::Text("- %s %f", system.Name.c_str(), system.System->GetDeltaTimeAndRestart());
+	}
+
 	ImGui::End();
 }
