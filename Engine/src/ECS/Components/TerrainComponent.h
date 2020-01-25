08c81c02a6028099dff9511f8a8be4de94f352be
bilek993 <jbilinskipl@gmail.com>
2020-01-25 13:44:02 +0100

    Added generating vertex buffer.

--- Engine/src/ECS/Components/TerrainComponent.h
+++ Engine/src/ECS/Components/TerrainComponent.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "../../Renderer/Vertex/FatVertex.h"
+#include "../../Renderer/VertexBuffer.h"
+#include "../../Renderer/IndexBuffer.h"
 
 struct TerrainComponent final
 {
 	std::string PathToHeightmap;
+	VertexBuffer<FatVertex> RenderVertexBuffer;
+	IndexBuffer RenderIndexBuffer;
 };
--- Engine/src/ECS/Systems/TerrainSystem.cpp
+++ Engine/src/ECS/Systems/TerrainSystem.cpp
@@ -1,4 +1,5 @@
 #include "TerrainSystem.h"
+#include "stb_image.h"
 
 void TerrainSystem::Start()
 {
@@ -16,5 +17,49 @@ void TerrainSystem::Update(float deltaTime)
 
 void TerrainSystem::GenerateTerrainMesh(TerrainComponent& terrainComponent) const
 {
+	auto width = 0;
+	auto height = 0;
+	auto numberOfChannels = 0;
 
+	const auto data = stbi_load(terrainComponent.PathToHeightmap.c_str(), &width, &height, &numberOfChannels, 1);
+	if (data == nullptr)
+	{
+		Logger::Error("Couldn't open heightmap file!");
+		stbi_image_free(data);
+		return;
+	}
+
+	if (numberOfChannels != 1)
+		Logger::Warning("Number of channels for heigtmap is equal to" + std::to_string(numberOfChannels) + ". It might be very problematic!");
+
+	std::vector<FatVertex> vertices;
+	std::vector<DWORD> indices;
+
+	for (auto y = 0; y < height; y++)
+	{
+		for (auto x = 0; x < width; x += numberOfChannels)
+		{
+			const auto pixelOffset = data + ((y * height) + x) * numberOfChannels;
+			const auto terrainHeight = static_cast<int>(pixelOffset[0]);
+
+			FatVertex vertex{};
+			vertex.Position = DirectX::XMFLOAT3(x, terrainHeight, y);
+			vertex.Normal = DirectX::XMFLOAT3(0, 1, 0); // TODO: Change this
+			vertex.Tangent = DirectX::XMFLOAT3(1, 0, 0); // TODO: Change this
+			vertex.TextureCoord = DirectX::XMFLOAT2(0, 0); // TODO: Change this
+
+			vertices.emplace_back(vertex);
+		}
+	}
+
+	// TODO: Add generating indices
+
+	auto hr = terrainComponent.RenderVertexBuffer.Initialize(DirectXDevice, vertices.data(), vertices.size());
+	if (FAILED(hr))
+		Logger::Error("Couldn't create terrain vertex buffer.");
+	/*hr = terrainComponent.RenderIndexBuffer.Initialize(DirectXDevice, indices.data(), indices.size());
+	if (FAILED(hr))
+		Logger::Error("Couldn't create terrain index buffer.");*/
+
+	stbi_image_free(data);
 }
