fb09b6a7fcbdf4d49813f33aa6b7a4a455edb86e
bilek993 <jbilinskipl@gmail.com>
2020-02-03 18:20:24 +0100

    Added comment describing each frustum plane.

--- Engine/src/ECS/Components/CameraComponent.h
+++ Engine/src/ECS/Components/CameraComponent.h
@@ -7,5 +7,11 @@ struct CameraComponent final
 	DirectX::XMMATRIX ProjectionMatrix;
 	float MaxRotationY;
 	float MinRotationY;
+	// 0 - Left Frustum Plane
+	// 1 - RightFrustum Plane
+	// 2 - Top Frustum Plane
+	// 3 - Bottom Frustum Plane
+	// 4 - Near Frustum Plane
+	// 5 - Far Frustum Plane
 	DirectX::XMVECTOR FrustumPlanes[6]{};
 };
