566e40deed11ac490c5aa105f3d62241ea46ceb8
bilek993 <jbilinskipl@gmail.com>
2020-02-05 17:21:47 +0100

    Designed terrain tessllation window with proper logic.

--- Engine/src/GUI/Windows/TerrainTessellationWindow.cpp
+++ Engine/src/GUI/Windows/TerrainTessellationWindow.cpp
@@ -4,5 +4,12 @@
 void TerrainTessellationWindow::Draw()
 {
 	ImGui::Begin("Terrain tessellation settings", &IsEnabled);
+
+	ImGui::SliderFloat("Minimal tessellation factor", &RenderSettings->MinTerrainTessellationFactor, 0.0f, 6.0f);
+	ImGui::SliderFloat("Maximum tessellation factor", &RenderSettings->MaxTerrainTessellationFactor, 0.0f, 6.0f);
+
+	ImGui::DragFloat("Minimum tessellation distance", &RenderSettings->MinTerrainTessellationDistance, 5.0f);
+	ImGui::DragFloat("Maximum tessellation distance", &RenderSettings->MaxTerrainTessellationDistance, 5.0f);
+
 	ImGui::End();
 }
