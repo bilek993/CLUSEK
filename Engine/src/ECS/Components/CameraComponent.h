71f5ae00097816df8f5998e68bcbd158bb36cae8
bilek993 <jbilinskipl@gmail.com>
2020-02-27 19:52:16 +0100

    Moved frustum planes documentation.

--- Engine/src/ECS/Components/CameraComponent.h
+++ Engine/src/ECS/Components/CameraComponent.h
@@ -8,11 +8,5 @@ struct CameraComponent final
 	DirectX::XMMATRIX ProjectionMatrix;
 	float MaxRotationY;
 	float MinRotationY;
-	// 0 - Left Frustum Plane
-	// 1 - RightFrustum Plane
-	// 2 - Top Frustum Plane
-	// 3 - Bottom Frustum Plane
-	// 4 - Near Frustum Plane
-	// 5 - Far Frustum Plane
 	std::array<DirectX::XMVECTOR,6> FrustumPlanes{};
 };
--- Engine/src/Renderer/FrustumUtil.h
+++ Engine/src/Renderer/FrustumUtil.h
@@ -2,6 +2,12 @@
 #include <DirectXMath.h>
 #include <array>
 
+// 0 - Left Frustum Plane
+// 1 - RightFrustum Plane
+// 2 - Top Frustum Plane
+// 3 - Bottom Frustum Plane
+// 4 - Near Frustum Plane
+// 5 - Far Frustum Plane
 class FrustumUtil final
 {
 public:
