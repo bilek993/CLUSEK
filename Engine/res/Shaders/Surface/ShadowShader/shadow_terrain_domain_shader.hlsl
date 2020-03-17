8943e75cdeb25c9025703d9daabfe18aba260fca
bilek993 <jbilinskipl@gmail.com>
2020-03-17 12:54:41 +0100

    Fixed sampler error.

--- Engine/res/Shaders/Surface/ShadowShader/shadow_terrain_domain_shader.hlsl
+++ Engine/res/Shaders/Surface/ShadowShader/shadow_terrain_domain_shader.hlsl
@@ -28,7 +28,7 @@ struct PatchTess
 };
 
 Texture2D HeightmapTexture : register(t0);
-SamplerState ClampSampler : register(s1);
+SamplerState TerrainSamplerState : register(s0);
 
 [domain("quad")]
 DS_OUTPUT main(PatchTess patchTess, float2 uv : SV_DomainLocation, const OutputPatch<DS_INPUT, 4> quad)
@@ -36,7 +36,7 @@ DS_OUTPUT main(PatchTess patchTess, float2 uv : SV_DomainLocation, const OutputP
     float3 position = lerp(lerp(quad[0].Position, quad[1].Position, uv.x), lerp(quad[2].Position, quad[3].Position, uv.x), uv.y);
     float2 coord = lerp(lerp(quad[0].TextureCoord, quad[1].TextureCoord, uv.x), lerp(quad[2].TextureCoord, quad[3].TextureCoord, uv.x), uv.y);
     
-    position.y = HeightmapTexture.SampleLevel(ClampSampler, coord, 0).r * MaxHeight;
+    position.y = HeightmapTexture.SampleLevel(TerrainSamplerState, coord, 0).r * MaxHeight;
     
     DS_OUTPUT output;
     output.Position = mul(float4(position, 1.0f), WorldLightMatrix);
