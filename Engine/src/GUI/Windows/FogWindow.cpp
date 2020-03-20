61f691bd2447d3af60caba6018dca45cc8f11123
bilek993 <jbilinskipl@gmail.com>
2020-03-20 15:26:50 +0100

    Added content into fog window.

--- Engine/src/GUI/Windows/FogWindow.cpp
+++ Engine/src/GUI/Windows/FogWindow.cpp
@@ -4,5 +4,13 @@
 void FogWindow::Draw()
 {
 	ImGui::Begin("Fog settings", &IsEnabled);
+	ImGui::Text("Fog color:");
+	ImGui::ColorPicker3("Color##Fog",
+		&RenderSettings->FogColor.x,
+		ImGuiColorEditFlags_NoAlpha |
+		ImGuiColorEditFlags_PickerHueBar |
+		ImGuiColorEditFlags_NoInputs);
+	ImGui::DragFloat("Fog density", &RenderSettings->FogDensity, 0.0001f, 0.0f, 0.0f, "%.5f");
+	ImGui::DragFloat("Sky constant value", &RenderSettings->SkyConstantValue, 1.0f);
 	ImGui::End();
 }
