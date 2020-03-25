4ca7afb4066b4108333069525e7c8bd3e3a5c48e
bilek993 <jbilinskipl@gmail.com>
2020-03-25 16:08:58 +0100

    Fog light color can be changed in real-time.

--- Engine/src/GUI/Windows/FogWindow.cpp
+++ Engine/src/GUI/Windows/FogWindow.cpp
@@ -4,14 +4,25 @@
 void FogWindow::Draw()
 {
 	ImGui::Begin("Fog settings", &IsEnabled);
+
 	ImGui::Text("Fog color:");
 	ImGui::ColorPicker3("Color##Fog",
-		&RenderSettings->FogColor.x,
-		ImGuiColorEditFlags_NoAlpha |
-		ImGuiColorEditFlags_PickerHueBar |
-		ImGuiColorEditFlags_NoInputs);
+						&RenderSettings->FogColor.x,
+						ImGuiColorEditFlags_NoAlpha |
+						ImGuiColorEditFlags_PickerHueBar |
+						ImGuiColorEditFlags_NoInputs);
+
 	ImGui::DragFloat("Fog density", &RenderSettings->FogDensity, 0.0001f, 0.0f, 0.0f, "%.5f");
+
+	ImGui::Text("Fog Light color:");
+	ImGui::ColorPicker3("Color##LightFog",
+						&RenderSettings->FogLightColor.x,
+						ImGuiColorEditFlags_NoAlpha |
+						ImGuiColorEditFlags_PickerHueBar |
+						ImGuiColorEditFlags_NoInputs);
+
 	ImGui::DragFloat("Fog minimal distance", &RenderSettings->FogMinDistance, 0.1f);
 	ImGui::DragFloat("Sky constant value", &RenderSettings->SkyConstantValue, 1.0f);
+
 	ImGui::End();
 }
