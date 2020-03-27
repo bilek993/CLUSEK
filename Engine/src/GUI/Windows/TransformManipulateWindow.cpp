c03b776c3dd0d9bb55b972890f57a1eac437f91b
bilek993 <jbilinskipl@gmail.com>
2020-03-27 11:03:47 +0100

    Added selectable combo list.

--- Engine/src/GUI/Windows/TransformManipulateWindow.cpp
+++ Engine/src/GUI/Windows/TransformManipulateWindow.cpp
@@ -1,11 +1,31 @@
 #include "TransformManipulateWindow.h"
 #include <imgui.h>
+#include "../../ECS/Components/ModelRenderComponent.h"
+#include "../../ECS/Components/SkyboxComponent.h"
+#include "../../ECS/Components/TerrainComponent.h"
 
 void TransformManipulateWindow::Draw()
 {
 	ImGui::Begin("Transform manipulation", &IsEnabled);
 
-	// TODO: Add code here
+	DrawCombo();
+	DrawDetails();
 
 	ImGui::End();
+}
+
+void TransformManipulateWindow::DrawCombo()
+{
+	std::string listIds{};
+	auto counter = 0;
+
+	ListComponent<ModelRenderComponent>(counter, listIds);
+	ListComponent<SkyboxComponent>(counter, listIds);
+	ListComponent<TerrainComponent>(counter, listIds);
+
+	ImGui::Combo("Selected entity Id", &SelectedId, listIds.c_str());
+}
+
+void TransformManipulateWindow::DrawDetails()
+{
 }
--- Engine/src/GUI/Windows/TransformManipulateWindow.h
+++ Engine/src/GUI/Windows/TransformManipulateWindow.h
@@ -5,4 +5,22 @@ class TransformManipulateWindow final : public BaseWindow
 {
 protected:
 	void Draw() override;
+private:
+	void DrawCombo();
+	void DrawDetails();
+
+	template <class T>
+	void ListComponent(int& counter, std::string& list);
+
+	int SelectedId = -1;
 };
+
+template <class T>
+void TransformManipulateWindow::ListComponent(int& counter, std::string& list)
+{
+	Registry->view<T>().each([this, &counter, &list](T& component)
+	{
+		counter++;
+		list += "Transformable entity " + std::to_string(counter) + '\0';
+	});
+}
