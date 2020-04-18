dfbcecc436d21727bbca06b46655a94e9db18f74
bilek993 <jbilinskipl@gmail.com>
2020-04-18 13:58:51 +0200

    Added proper filters parsing.

--- Engine/src/Utils/TextureBlockCompression.cpp
+++ Engine/src/Utils/TextureBlockCompression.cpp
@@ -38,7 +38,7 @@ void TextureBlockCompression::Compress(	bool* loadingState,
 	hr = GenerateMipMaps(	inputImage.GetImages(), 
 							inputImage.GetImageCount(), 
 							inputImage.GetMetadata(), 
-							DirectX::TEX_FILTER_DEFAULT, 
+							GenerateFlagsForMipMaps(compressionSettings),
 							0, 
 							inputImageWithMipMaps);
 	if (FAILED(hr))
@@ -55,8 +55,8 @@ void TextureBlockCompression::Compress(	bool* loadingState,
 	hr = DirectX::Compress(	inputImageWithMipMaps.GetImages(),
 							inputImageWithMipMaps.GetImageCount(),
 							inputImageWithMipMaps.GetMetadata(),
-							DXGI_FORMAT_BC1_UNORM,
-							DirectX::TEX_COMPRESS_DEFAULT,
+							GenerateCompressionFormat(compressionSettings),
+							GenerateFlagsForCompression(compressionSettings),
 							compressionSettings->Threshold,
 							outputImage);
 	if (FAILED(hr))
@@ -73,7 +73,7 @@ void TextureBlockCompression::Compress(	bool* loadingState,
 	hr = SaveToDDSFile(	outputImage.GetImages(), 
 						outputImage.GetImageCount(), 
 						outputImage.GetMetadata(), 
-						DirectX::DDS_FLAGS_NONE, 
+						GenerateFlagsForOutput(compressionSettings),
 						StringUtil::StringToWide(*outputPath).c_str());
 	if (FAILED(hr))
 	{
@@ -109,6 +109,95 @@ DWORD TextureBlockCompression::GenerateFlagsForInput(const TextureBlockCompressi
 	HANDLE_FLAG(flags, WIC_FLAGS_FILTER_LINEAR, compressionSettings);
 	HANDLE_FLAG(flags, WIC_FLAGS_FILTER_CUBIC, compressionSettings);
 	HANDLE_FLAG(flags, WIC_FLAGS_FILTER_FANT, compressionSettings);
+
+	return flags;
+}
+
+DWORD TextureBlockCompression::GenerateFlagsForMipMaps(const TextureBlockCompressionSettings* compressionSettings)
+{
+	DWORD flags = 0;
+
+	HANDLE_FLAG(flags, TEX_FILTER_DEFAULT, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_POINT, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_LINEAR, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_CUBIC, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_FANT, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_BOX, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_TRIANGLE, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_DITHER, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_DITHER_DIFFUSION, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_WRAP_U, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_WRAP_V, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_WRAP_W, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_MIRROR_U, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_MIRROR_V, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_MIRROR_W, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_SRGB_IN, compressionSettings);
+	HANDLE_FLAG(flags, TEX_FILTER_SRGB_OUT, compressionSettings);
+
+	return flags;
+}
+
+DXGI_FORMAT TextureBlockCompression::GenerateCompressionFormat(const TextureBlockCompressionSettings* compressionSettings)
+{
+	HANDLE_FORMAT(DXGI_FORMAT_BC1_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC1_UNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC1_UNORM_SRGB, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC2_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC2_UNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC2_UNORM_SRGB, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC3_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC3_UNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC3_UNORM_SRGB, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC4_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC4_UNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC4_SNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC5_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC5_UNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC5_SNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC6H_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC6H_UF16, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC6H_SF16, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC7_TYPELESS, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC7_UNORM, compressionSettings->CompressAlgorithm);
+	HANDLE_FORMAT(DXGI_FORMAT_BC7_UNORM_SRGB, compressionSettings->CompressAlgorithm);
+
+	Logger::Error("Unknown compression format!");
+	return DXGI_FORMAT_UNKNOWN;
+}
+
+DWORD TextureBlockCompression::GenerateFlagsForCompression(const TextureBlockCompressionSettings* compressionSettings)
+{
+	DWORD flags = 0;
+
+	HANDLE_FLAG(flags, TEX_COMPRESS_DEFAULT, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_RGB_DITHER, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_A_DITHER, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_DITHER, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_UNIFORM, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_PARALLEL, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_SRGB_IN, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_SRGB_OUT, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_BC7_USE_3SUBSETS, compressionSettings);
+	HANDLE_FLAG(flags, TEX_COMPRESS_BC7_QUICK, compressionSettings);
+
+	return flags;
+}
+
+DWORD TextureBlockCompression::GenerateFlagsForOutput(const TextureBlockCompressionSettings* compressionSettings)
+{
+	DWORD flags = 0;
+
+	HANDLE_FLAG(flags, DDS_FLAGS_NONE, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_LEGACY_DWORD, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_BAD_DXTN_TAILS, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_NO_LEGACY_EXPANSION, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_NO_R10B10G10A2_FIXUP, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_FORCE_RGB, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_NO_16BPP, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_EXPAND_LUMINANCE, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_FORCE_DX10_EXT, compressionSettings);
+	HANDLE_FLAG(flags, DDS_FLAGS_FORCE_DX10_EXT_MISC2, compressionSettings);
 
 	return flags;
 }
--- Engine/src/Utils/TextureBlockCompression.h
+++ Engine/src/Utils/TextureBlockCompression.h
@@ -4,6 +4,7 @@
 #include "TextureBlockCompressionSettings.h"
 
 #define HANDLE_FLAG(FLAG_VAR, FLAG_VAL, SETTINGS) if (SETTINGS->FLAG_VAL) FLAG_VAR |= DirectX::FLAG_VAL
+#define HANDLE_FORMAT(FORMAT, INPUT) if (INPUT == #FORMAT) return FORMAT;
 
 class TextureBlockCompression final
 {
@@ -14,29 +15,33 @@ public:
 							const TextureBlockCompressionSettings* compressionSettings);
 
 	inline static const char* const SUPPORTED_ALGORITHMS[] = {
-		"BC1_TYPELESS",
-		"BC1_UNORM",
-		"BC1_UNORM_SRGB",
-		"BC2_TYPELESS",
-		"BC2_UNORM",
-		"BC2_UNORM_SRGB",
-		"BC3_TYPELESS",
-		"BC3_UNORM",
-		"BC3_UNORM_SRGB",
-		"BC4_TYPELESS",
-		"BC4_UNORM",
-		"BC4_SNORM",
-		"BC5_TYPELESS",
-		"BC5_UNORM",
-		"BC5_SNORM",
-		"BC6H_TYPELESS",
-		"BC6H_UF16",
-		"BC6H_SF16",
-		"BC7_TYPELESS",
-		"BC7_UNORM",
-		"BC7_UNORM_SRGB",
+		"DXGI_FORMAT_BC1_TYPELESS",
+		"DXGI_FORMAT_BC1_UNORM",
+		"DXGI_FORMAT_BC1_UNORM_SRGB",
+		"DXGI_FORMAT_BC2_TYPELESS",
+		"DXGI_FORMAT_BC2_UNORM",
+		"DXGI_FORMAT_BC2_UNORM_SRGB",
+		"DXGI_FORMAT_BC3_TYPELESS",
+		"DXGI_FORMAT_BC3_UNORM",
+		"DXGI_FORMAT_BC3_UNORM_SRGB",
+		"DXGI_FORMAT_BC4_TYPELESS",
+		"DXGI_FORMAT_BC4_UNORM",
+		"DXGI_FORMAT_BC4_SNORM",
+		"DXGI_FORMAT_BC5_TYPELESS",
+		"DXGI_FORMAT_BC5_UNORM",
+		"DXGI_FORMAT_BC5_SNORM",
+		"DXGI_FORMAT_BC6H_TYPELESS",
+		"DXGI_FORMAT_BC6H_UF16",
+		"DXGI_FORMAT_BC6H_SF16",
+		"DXGI_FORMAT_BC7_TYPELESS",
+		"DXGI_FORMAT_BC7_UNORM",
+		"DXGI_FORMAT_BC7_UNORM_SRGB",
 	};
 
 private:
 	static DWORD GenerateFlagsForInput(const TextureBlockCompressionSettings* compressionSettings);
+	static DWORD GenerateFlagsForMipMaps(const TextureBlockCompressionSettings* compressionSettings);
+	static DXGI_FORMAT GenerateCompressionFormat(const TextureBlockCompressionSettings* compressionSettings);
+	static DWORD GenerateFlagsForCompression(const TextureBlockCompressionSettings* compressionSettings);
+	static DWORD GenerateFlagsForOutput(const TextureBlockCompressionSettings* compressionSettings);
 };
