478de2a99fb617ca0d48e48e63180057405b5eb6
bilek993 <jbilinskipl@gmail.com>
2020-05-20 12:27:42 +0200

    Added lod transition field and loader for model render component.

--- Engine/src/ECS/Components/ModelRenderComponent.h
+++ Engine/src/ECS/Components/ModelRenderComponent.h
@@ -5,6 +5,7 @@
 struct ModelRenderComponent final
 {
 	float LowPolyDistance = -1.0f;
+	float LodTransitionDistance = 15.0f;
 	std::string ModelId;
 	std::string LowPolyModelId;
 	std::string MaterialId;
