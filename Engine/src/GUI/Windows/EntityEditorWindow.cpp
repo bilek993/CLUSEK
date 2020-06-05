eee947f912f1e7022f4ad8bdbb7881a10272ed0d
bilek993 <jbilinskipl@gmail.com>
2020-06-05 21:35:48 +0200

    Connected all components editors.

--- Engine/src/GUI/Windows/EntityEditorWindow.cpp
+++ Engine/src/GUI/Windows/EntityEditorWindow.cpp
@@ -1,5 +1,24 @@
 #include "EntityEditorWindow.h"
 #include "../../ECS/Components/TransformComponent.h"
+#include "../../ECS/Components/CameraComponent.h"
+#include "../../ECS/Components/CameraTargetComponent.h"
+#include "../../ECS/Components/CameraVehicleAnimatedTargetComponent.h"
+#include "../../ECS/Components/ModelRenderComponent.h"
+#include "../../ECS/Components/PhysicsMaterialComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicBoxComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicCapsuleComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicCylinderComponent.h"
+#include "../../ECS/Components/RigidbodyDynamicSphereComponent.h"
+#include "../../ECS/Components/RigidbodyStaticBoxComponent.h"
+#include "../../ECS/Components/RigidbodyStaticCapsuleComponent.h"
+#include "../../ECS/Components/RigidbodyStaticCylinderComponent.h"
+#include "../../ECS/Components/RigidbodyStaticHeightFieldsComponent.h"
+#include "../../ECS/Components/RigidbodyStaticPlaneComponent.h"
+#include "../../ECS/Components/RigidbodyStaticSphereComponent.h"
+#include "../../ECS/Components/SkyboxComponent.h"
+#include "../../ECS/Components/TerrainComponent.h"
+#include "../../ECS/Components/VehicleComponent.h"
+#include "../../ECS/Components/VehiclePlayerControllerComponent.h"
 
 void EntityEditorWindow::Draw()
 {
@@ -15,7 +34,27 @@ void EntityEditorWindow::Draw()
 
 void EntityEditorWindow::DrawComponentsForEntity()
 {
+	REGISTER_COMPONENT_EDITOR(CameraComponent, "Camera Component");
+	REGISTER_COMPONENT_EDITOR(CameraTargetComponent, "Camera Target Component");
+	REGISTER_COMPONENT_EDITOR(CameraVehicleAnimatedTargetComponent, "Camera Vehicle Animated Target Component");
+	REGISTER_COMPONENT_EDITOR(ModelRenderComponent, "Model Render Component");
+	REGISTER_COMPONENT_EDITOR(PhysicsMaterialComponent, "Physics Material Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyDynamicBoxComponent, "Rigidbody Dynamic Box Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyDynamicCapsuleComponent, "Rigidbody Dynamic Capsule Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyDynamicCylinderComponent, "Rigidbody Dynamic Cylinder Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyDynamicSphereComponent, "Rigidbody Dynamic Sphere Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyStaticBoxComponent, "Rigidbody Static Box Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyStaticCapsuleComponent, "Rigidbody Static Capsule Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyStaticCylinderComponent, "Rigidbody Static Cylinder Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyStaticHeightFieldsComponent, "Rigidbody Static Height Fields Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyStaticPlaneComponent, "Rigidbody Static Plane Component");
+	REGISTER_COMPONENT_EDITOR(RigidbodyStaticSphereComponent, "Rigidbody Static Sphere Component");
+	REGISTER_COMPONENT_EDITOR(SkyboxComponent, "Skybox Component");
+	REGISTER_COMPONENT_EDITOR(TerrainComponent, "Terrain Component");
 	REGISTER_COMPONENT_EDITOR(TransformComponent, "Transform Component");
+	REGISTER_COMPONENT_EDITOR(VehicleComponent, "Vehicle Component");
+	REGISTER_COMPONENT_EDITOR(VehiclePlayerControllerComponent, "Vehicle Player Controller Component");
+	REGISTER_COMPONENT_EDITOR(WheelComponent, "Wheel Component");
 }
 
 void EntityEditorWindow::DrawNoElements() const
--- Engine/src/GUI/Windows/EntityEditorWindow.h
+++ Engine/src/GUI/Windows/EntityEditorWindow.h
@@ -2,6 +2,26 @@
 #include "BaseWindow.h"
 #include <imgui.h>
 #include "../Components/TransformComponentEditor.h"
+#include "../Components/CameraComponentEditor.h"
+#include "../Components/CameraTargetComponentEditor.h"
+#include "../Components/CameraVehicleAnimatedTargetComponentEditor.h"
+#include "../Components/ModelRenderComponentEditor.h"
+#include "../Components/PhysicsMaterialComponentEditor.h"
+#include "../Components/RigidbodyDynamicBoxComponentEditor.h"
+#include "../Components/RigidbodyDynamicCapsuleComponentEditor.h"
+#include "../Components/RigidbodyDynamicCylinderComponentEditor.h"
+#include "../Components/RigidbodyDynamicSphereComponentEditor.h"
+#include "../Components/RigidbodyStaticBoxComponentEditor.h"
+#include "../Components/RigidbodyStaticCapsuleComponentEditor.h"
+#include "../Components/RigidbodyStaticCylinderComponentEditor.h"
+#include "../Components/RigidbodyStaticHeightFieldsComponentEditor.h"
+#include "../Components/RigidbodyStaticPlaneComponentEditor.h"
+#include "../Components/RigidbodyStaticSphereComponentEditor.h"
+#include "../Components/SkyboxComponentEditor.h"
+#include "../Components/TerrainComponentEditor.h"
+#include "../Components/VehicleComponentEditor.h"
+#include "../Components/VehiclePlayerControllerComponentEditor.h"
+#include "../Components/WheelComponentEditor.h"
 
 #define REGISTER_COMPONENT_EDITOR(COMPONENT_CLASS, COMPONENT_NAME) DrawComponentDetails<COMPONENT_CLASS>(SelectedEntity, COMPONENT_CLASS##EditorInstance, COMPONENT_NAME)
 
@@ -17,7 +37,27 @@ private:
 	template <class T>
 	void DrawComponentDetails(const entt::entity* entity, BaseComponentEditor& editorInstance, const std::string& name) const;
 
+	CameraComponentEditor CameraComponentEditorInstance;
+	CameraTargetComponentEditor CameraTargetComponentEditorInstance;
+	CameraVehicleAnimatedTargetComponentEditor CameraVehicleAnimatedTargetComponentEditorInstance;
+	ModelRenderComponentEditor ModelRenderComponentEditorInstance;
+	PhysicsMaterialComponentEditor PhysicsMaterialComponentEditorInstance;
+	RigidbodyDynamicBoxComponentEditor RigidbodyDynamicBoxComponentEditorInstance;
+	RigidbodyDynamicCapsuleComponentEditor RigidbodyDynamicCapsuleComponentEditorInstance;
+	RigidbodyDynamicCylinderComponentEditor RigidbodyDynamicCylinderComponentEditorInstance;
+	RigidbodyDynamicSphereComponentEditor RigidbodyDynamicSphereComponentEditorInstance;
+	RigidbodyStaticBoxComponentEditor RigidbodyStaticBoxComponentEditorInstance;
+	RigidbodyStaticCapsuleComponentEditor RigidbodyStaticCapsuleComponentEditorInstance;
+	RigidbodyStaticCylinderComponentEditor RigidbodyStaticCylinderComponentEditorInstance;
+	RigidbodyStaticHeightFieldsComponentEditor RigidbodyStaticHeightFieldsComponentEditorInstance;
+	RigidbodyStaticPlaneComponentEditor RigidbodyStaticPlaneComponentEditorInstance;
+	RigidbodyStaticSphereComponentEditor RigidbodyStaticSphereComponentEditorInstance;
+	SkyboxComponentEditor SkyboxComponentEditorInstance;
+	TerrainComponentEditor TerrainComponentEditorInstance;
 	TransformComponentEditor TransformComponentEditorInstance;
+	VehicleComponentEditor VehicleComponentEditorInstance;
+	VehiclePlayerControllerComponentEditor VehiclePlayerControllerComponentEditorInstance;
+	WheelComponentEditor WheelComponentEditorInstance;
 };
 
 template <class T>
