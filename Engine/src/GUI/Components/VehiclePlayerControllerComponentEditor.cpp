53e6c2613c7baeefa1786b45f3085c162a3b5369
bilek993 <jbilinskipl@gmail.com>
2020-06-11 18:40:41 +0200

    Added editor for VehiclePlayerControllerComponent.

--- Engine/src/GUI/Components/VehiclePlayerControllerComponentEditor.cpp
+++ Engine/src/GUI/Components/VehiclePlayerControllerComponentEditor.cpp
@@ -1,1 +1,11 @@
 #include "VehiclePlayerControllerComponentEditor.h"
+
+void VehiclePlayerControllerComponentEditor::Draw()
+{
+	const auto component = GetPointerToThisComponent();
+
+	ImGui::Checkbox("Possessed", &component->Possessed);
+	ImGui::InputFloat("Steering speed", &component->SteeringSpeed);
+	ImGui::InputFloat("Wheel returning to neutral position", &component->WheelReturningToNeutralPosition, 0, 0, "%.5f");
+	ImGui::InputFloat("Minimal speed for gear change from or to reverse", &component->MinimalSpeedForGearChangeFromOrToReverse);
+}
--- Engine/src/GUI/Components/VehiclePlayerControllerComponentEditor.h
+++ Engine/src/GUI/Components/VehiclePlayerControllerComponentEditor.h
@@ -4,4 +4,6 @@
 
 class VehiclePlayerControllerComponentEditor final : public BaseComponentEditor<VehiclePlayerControllerComponent>
 {
+protected:
+	void Draw() override;
 };
