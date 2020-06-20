bbf1b1e8b8bbb6e5ef3f245377354b57ce3bf7e3
bilek993 <jbilinskipl@gmail.com>
2020-06-20 14:48:43 +0200

    Added component editor for RigidbodyStaticHeightFieldsComponent.

--- Engine/src/GUI/Components/RigidbodyStaticHeightFieldsComponentEditor.cpp
+++ Engine/src/GUI/Components/RigidbodyStaticHeightFieldsComponentEditor.cpp
@@ -1,1 +1,20 @@
 #include "RigidbodyStaticHeightFieldsComponentEditor.h"
+
+void RigidbodyStaticHeightFieldsComponentEditor::Draw()
+{
+	const auto component = GetPointerToThisComponent();
+
+	physx::PxHeightFieldGeometry geometry;
+	component->Shape->getHeightFieldGeometry(geometry);
+
+	DrawGeometryParameters(geometry);
+
+	component->Shape->setGeometry(geometry);
+}
+
+void RigidbodyStaticHeightFieldsComponentEditor::DrawGeometryParameters(physx::PxHeightFieldGeometry& geometry) const
+{
+	ImGui::InputFloat("Column scale", &geometry.columnScale);
+	ImGui::InputFloat("Height", &geometry.heightScale);
+	ImGui::InputFloat("Row scale", &geometry.rowScale);
+}
--- Engine/src/GUI/Components/RigidbodyStaticHeightFieldsComponentEditor.h
+++ Engine/src/GUI/Components/RigidbodyStaticHeightFieldsComponentEditor.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "BaseComponentEditor.h"
 #include "../../ECS/Components/RigidbodyStaticHeightFieldsComponent.h"
+#include <PxPhysicsAPI.h>
 
 class RigidbodyStaticHeightFieldsComponentEditor final : public BaseComponentEditor<RigidbodyStaticHeightFieldsComponent>
 {
+protected:
+	void Draw() override;
+
+private:
+	void DrawGeometryParameters(physx::PxHeightFieldGeometry& geometry) const;
 };
