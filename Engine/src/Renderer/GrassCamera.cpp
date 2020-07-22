9774f564e8102baf2a30c063659754f9065ab948
bilek993 <jbilinskipl@gmail.com>
2020-07-22 20:31:36 +0200

    Fixed code formatting.

--- Engine/src/Renderer/GrassCamera.cpp
+++ Engine/src/Renderer/GrassCamera.cpp
@@ -3,15 +3,15 @@
 DirectX::XMMATRIX GrassCamera::GenerateCameraMatrix(const TransformComponent& mainCameraTransformComponent) // TODO: Fix this function
 {
 	const auto viewMatrix = DirectX::XMMatrixLookAtLH(	ZeroVector, 
-																	DownVector, 
-																	UpVector);
+														DownVector, 
+														UpVector);
 	
 	const auto projectionMatrix = DirectX::XMMatrixOrthographicOffCenterLH(	-500.0f, 
-																						500.0f, 
-																						-500.0f, 
-																						500.0f, 
-																						-10000.0f, 
-																						10000.0f);
+																			500.0f, 
+																			-500.0f, 
+																			500.0f, 
+																			-10000.0f, 
+																			10000.0f);
 	
 	return viewMatrix * projectionMatrix;
 }
