fdea8209ddf32cbd3f9ee64054399bf815d1709f
bilek993 <jbilinskipl@gmail.com>
2020-07-31 15:00:57 +0200

    Added grass resource load function.

--- Engine/src/Loaders/ModelLoader.cpp
+++ Engine/src/Loaders/ModelLoader.cpp
@@ -1,10 +1,8 @@
 #include "ModelLoader.h"
-#include <assimp/Importer.hpp>
-#include <assimp/postprocess.h>
-#include <assimp/scene.h>
 #include "../Renderer/FrustumUtil.h"
 
 std::unordered_map<std::string, std::shared_ptr<std::vector<Mesh>>> ModelLoader::MeshesResources;
+std::unordered_map<std::string, std::shared_ptr<std::vector<GrassMesh>>> ModelLoader::GrassMeshesResources;
 
 void ModelLoader::LoadResource(ID3D11Device *device, const std::string& path, const std::string& resourceId)
 {
@@ -78,6 +76,80 @@ void ModelLoader::LoadResource(ID3D11Device *device, const std::string& path, co
 
 	ResourcesMapMutex.lock();
 	MeshesResources[resourceId] = loadedMeshes;
+	ResourcesMapMutex.unlock();
+}
+
+void ModelLoader::LoadGrassResource(ID3D11Device* device, const std::string& path, const std::string& resourceId)
+{
+	Logger::Debug("Preparing to load grass mesh from: '" + path + "'...");
+
+	auto loadedMeshes = std::make_shared<std::vector<GrassMesh>>();
+	Assimp::Importer importer;
+
+	Logger::Debug("Reading data from file...");
+	const auto scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_ConvertToLeftHanded | aiProcess_CalcTangentSpace);
+	if (scene == nullptr)
+		return;
+
+	const auto meshes = scene->mMeshes;
+	for (unsigned int i = 0; i < scene->mNumMeshes; i++)
+	{
+		auto mesh = meshes[i];
+		std::vector<FatVertex> vertices;
+		std::vector<DWORD> indices;
+
+		for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+		{
+			FatVertex vertex;
+
+			vertex.Position.x = mesh->mVertices[j].x;
+			vertex.Position.y = mesh->mVertices[j].y;
+			vertex.Position.z = mesh->mVertices[j].z;
+
+			vertex.Normal.x = mesh->mNormals[j].x;
+			vertex.Normal.y = mesh->mNormals[j].y;
+			vertex.Normal.z = mesh->mNormals[j].z;
+
+			vertex.Tangent.x = mesh->mTangents[j].x;
+			vertex.Tangent.y = mesh->mTangents[j].y;
+			vertex.Tangent.z = mesh->mTangents[j].z;
+
+			if (mesh->mTextureCoords[0])
+			{
+				vertex.TextureCoord.x = static_cast<float>(mesh->mTextureCoords[0][j].x);
+				vertex.TextureCoord.y = static_cast<float>(mesh->mTextureCoords[0][j].y);
+			}
+
+			vertices.emplace_back(vertex);
+		}
+
+		for (unsigned int j = 0; j < mesh->mNumFaces; j++)
+		{
+			const auto face = mesh->mFaces[j];
+
+			for (unsigned int k = 0; k < face.mNumIndices; k++)
+			{
+				indices.emplace_back(face.mIndices[k]);
+			}
+		}
+
+		GrassMesh newMesh;
+		newMesh.Name = mesh->mName.C_Str();
+
+		auto hr = newMesh.RenderIndexBuffer.Initialize(device, indices.data(), indices.size());
+		if (FAILED(hr))
+			continue;
+		hr = newMesh.RenderVertexBuffer.Initialize(device, vertices.data(), vertices.size());
+		if (FAILED(hr))
+			continue;
+
+		loadedMeshes->emplace_back(newMesh);
+
+		Logger::Debug("Grass mesh '" + newMesh.Name + "' added into the model!");
+	}
+
+	ResourcesMapMutex.lock();
+	GrassMeshesResources[resourceId] = loadedMeshes;
 	ResourcesMapMutex.unlock();
 }
 
--- Engine/src/Loaders/ModelLoader.h
+++ Engine/src/Loaders/ModelLoader.h
@@ -1,17 +1,24 @@
 #pragma once
 #include <vector>
-#include "../Renderer/Mesh.h"
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 #include <unordered_map>
 #include <mutex>
+#include "../Renderer/Mesh.h"
+#include "../Renderer/GrassMesh.h"
 
 class ModelLoader final
 {
 public:
 	static void LoadResource(ID3D11Device *device, const std::string& path, const std::string& resourceId);
+	static void LoadGrassResource(ID3D11Device *device, const std::string& path, const std::string& resourceId);
+	
 	static std::shared_ptr<std::vector<Mesh>> GetResource(const std::string& resourceId);
 
 private:
 	static inline std::mutex ResourcesMapMutex{};
 
 	static std::unordered_map<std::string, std::shared_ptr<std::vector<Mesh>>> MeshesResources;
+	static std::unordered_map<std::string, std::shared_ptr<std::vector<GrassMesh>>> GrassMeshesResources;
 };
