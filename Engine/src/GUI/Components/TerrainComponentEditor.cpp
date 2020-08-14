0dddf2b7dce778a616e082630dd0af67a211284d
bilek993 <jbilinskipl@gmail.com>
2020-08-14 19:37:42 +0200

    GrassPlacementThreshold can be changed from UI.

--- Engine/src/GUI/Components/TerrainComponentEditor.cpp
+++ Engine/src/GUI/Components/TerrainComponentEditor.cpp
@@ -7,6 +7,12 @@ void TerrainComponentEditor::Draw()
 	ImGui::SliderFloat("Minimal tessellation factor", &component->MinTerrainTessellationFactor, 0.0f, 6.0f);
 	ImGui::SliderFloat("Maximum tessellation factor", &component->MaxTerrainTessellationFactor, 0.0f, 6.0f);
 
+	ImGui::Separator();
+
 	ImGui::DragFloat("Minimum tessellation distance", &component->MinTerrainTessellationDistance, 5.0f);
 	ImGui::DragFloat("Maximum tessellation distance", &component->MaxTerrainTessellationDistance, 5.0f);
+
+	ImGui::Separator();
+
+	ImGui::DragFloat("Grass placement threshold", &component->GrassPlacementThreshold, 5.0f);
 }
