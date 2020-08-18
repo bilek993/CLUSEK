31459f9f88882e4abd868564598ef5eae9c5e0e5
bilek993 <jbilinskipl@gmail.com>
2020-08-18 15:59:29 +0200

    Grass random translation can be changed from the UI.

--- Engine/src/GUI/Windows/GrassWindow.cpp
+++ Engine/src/GUI/Windows/GrassWindow.cpp
@@ -13,6 +13,13 @@ void GrassWindow::Draw()
 	ImGui::InputFloat("Generator surface width", &RenderSettings->GrassGeneratorSurfaceWidth);
 	ImGui::InputFloat("Generator surface height", &RenderSettings->GrassGeneratorSurfaceHeight);
 	ImGui::InputFloat("Generator max camera distance", &RenderSettings->GrassGeneratorMaxCameraDistance);
+
+	ImGui::Separator();
+
+	ImGui::InputFloat("Min grass translation X", &RenderSettings->MinGrassTranslationX);
+	ImGui::InputFloat("Min grass translation Y", &RenderSettings->MinGrassTranslationY);
+	ImGui::InputFloat("Max grass translation X", &RenderSettings->MaxGrassTranslationX);
+	ImGui::InputFloat("Max grass translation Y", &RenderSettings->MaxGrassTranslationY);
 	
 	ImGui::End();
 }
