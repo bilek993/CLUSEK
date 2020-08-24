27def1e80fa8d51dd933764a2134a0857ad11d4e
bilek993 <jbilinskipl@gmail.com>
2020-08-24 17:21:32 +0200

    Added loading wind parameters from map file.

--- Engine/src/Loaders/Components/GrassComponentLoader.cpp
+++ Engine/src/Loaders/Components/GrassComponentLoader.cpp
@@ -13,6 +13,20 @@ void GrassComponentLoader::Add(nlohmann::json& json, entt::registry& registry, c
 	MAP_LOADER_BASIC_FIELD(MetalicInterpolationTarget, json, component, float);
 	MAP_LOADER_BASIC_FIELD(MetalicInterpolationRatio, json, component, float);
 	MAP_LOADER_BASIC_FIELD(OcclusionValue, json, component, float);
+	MAP_LOADER_BASIC_FIELD(WindVertexAffectHeight, json, component, float);
+	MAP_LOADER_BASIC_FIELD(GlobalWindSpeed, json, component, float);
+	MAP_LOADER_BASIC_FIELD(GlobalWindScale, json, component, float);
+	MAP_LOADER_BASIC_FIELD(LocalWindSpeed, json, component, float);
+
+	if (!json["LocalWindScaleX"].is_null() && !json["LocalWindScaleY"].is_null() && !json["LocalWindScaleZ"].is_null())
+	{
+		const auto x = json["LocalWindScaleX"].get<float>();
+		const auto y = json["LocalWindScaleY"].get<float>();
+		const auto z = json["LocalWindScaleZ"].get<float>();
+
+		component.LocalWindScale = DirectX::XMFLOAT3(x, y, z);
+	}
+	
 	MAP_LOADER_BASIC_FIELD(ModelId, json, component, std::string);
 	MAP_LOADER_BASIC_FIELD(MaterialId, json, component, std::string);
 }
