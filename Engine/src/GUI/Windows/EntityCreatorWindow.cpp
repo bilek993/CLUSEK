19ab39ee9f6b2807122efab5ed97907544888d20
bilek993 <jbilinskipl@gmail.com>
2020-09-26 10:23:10 +0200

    Added text field for json in entity creator window.

--- Engine/src/GUI/Windows/EntityCreatorWindow.cpp
+++ Engine/src/GUI/Windows/EntityCreatorWindow.cpp
@@ -1,11 +1,33 @@
 #include "EntityCreatorWindow.h"
 #include <imgui.h>
+#include "../../ECS/Components/TransformComponent.h"
+#include "../../Utils/ImGuiExtensions.h"
 
 void EntityCreatorWindow::Draw()
 {
 	ImGui::Begin("Entity creator", &IsEnabled);
 
-	// TODO: Add implementation here!
+	ImGui::InputTextMultiline(	"##EntityJsonText", 
+								const_cast<char*>(EntityJsonText.c_str()),
+								EntityJsonText.capacity() + 1,
+								ImVec2(-FLT_MIN, 350.0f),
+								ImGuiInputTextFlags_CallbackResize,
+								ImGuiExtensions::InputTextStringCallback,
+								&EntityJsonText);
+	
+	if (ImGui::Button("Create"))
+		CreateEntityWithComponents();
 
 	ImGui::End();
+}
+
+void EntityCreatorWindow::CreateEntityWithComponents()
+{
+	const auto entity = Registry->create();
+
+	// TODO: Add code here
+
+	Logger::Debug("New entity has been created!");
+
+	EntityJsonText = "";
 }
--- Engine/src/GUI/Windows/EntityCreatorWindow.h
+++ Engine/src/GUI/Windows/EntityCreatorWindow.h
@@ -5,4 +5,9 @@ class EntityCreatorWindow final : public BaseWindow
 {
 protected:
 	void Draw() override;
+
+private:
+	void CreateEntityWithComponents();
+	
+	std::string EntityJsonText{};
 };
