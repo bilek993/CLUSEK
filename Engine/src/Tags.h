473e1cc59fc735c3f17782f920154aa4e00608bb
bilek993 <jbilinskipl@gmail.com>
2020-09-26 15:32:06 +0200

    Added new internal tag.

--- Engine/src/Tags.h
+++ Engine/src/Tags.h
@@ -3,5 +3,7 @@
 
 struct Tags final
 {
+	inline static const entt::hashed_string::hash_type REQUIRES_REBUILD = "Requires rebuild"_hs; // This tag is used only internally
+	
 	inline static const entt::hashed_string::hash_type MAIN_CAMERA = "Main Camera"_hs;
 };
