516b43517d58cd67d8024e6a05d1fb8251ca9f45
bilek993 <jbilinskipl@gmail.com>
2020-10-02 19:29:25 +0200

    Fixed camera rotation from file.

--- Engine/src/ECS/Systems/TransformSystem.cpp
+++ Engine/src/ECS/Systems/TransformSystem.cpp
@@ -2,6 +2,7 @@
 #include "../../Tags.h"
 #include "../Components/TransformComponent.h"
 #include "../../Renderer/TransformLogic.h"
+#include "../Components/CameraComponent.h"
 #include "../Components/CameraTargetComponent.h"
 
 void TransformSystem::Start()
@@ -14,6 +15,17 @@ void TransformSystem::Rebuild()
 
 	Registry->view<TransformComponent, entt::tag<Tags::REQUIRES_REBUILD>>().each([](TransformComponent &transformComponent, auto _)
 	{
+		TransformLogic::SetPositionAndRotationEuler(transformComponent.InitialPositionVector,
+													transformComponent.InitialRotationVector,
+													transformComponent);
+	});
+
+	Registry->view<TransformComponent, CameraComponent, entt::tag<Tags::REQUIRES_REBUILD>>().each([](TransformComponent &transformComponent, CameraComponent &cameraComponent, auto _)
+	{
+		transformComponent.InitialRotationVector.x *= M_PI / 180.0f;
+		transformComponent.InitialRotationVector.y *= M_PI / 180.0f;
+		transformComponent.InitialRotationVector.z *= M_PI / 180.0f;
+		
 		TransformLogic::SetPositionAndRotationEuler(transformComponent.InitialPositionVector,
 													transformComponent.InitialRotationVector,
 													transformComponent);
