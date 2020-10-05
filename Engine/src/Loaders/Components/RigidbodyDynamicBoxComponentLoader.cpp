590ed9a07a7b470bf4e32f91b66068536286ed8e
bilek993 <jbilinskipl@gmail.com>
2020-10-05 18:43:57 +0200

    Position and rotation offset can be set in dynamic box component.

--- Engine/src/Loaders/Components/RigidbodyDynamicBoxComponentLoader.cpp
+++ Engine/src/Loaders/Components/RigidbodyDynamicBoxComponentLoader.cpp
@@ -10,4 +10,26 @@ void RigidbodyDynamicBoxComponentLoader::Add(nlohmann::json& json, entt::registr
 	MAP_LOADER_BASIC_FIELD(Height, json, component, float);
 	MAP_LOADER_BASIC_FIELD(Depth, json, component, float);
 	MAP_LOADER_BASIC_FIELD(Density, json, component, float);
+
+	if (!json["PositionOffsetX"].is_null() && !json["PositionOffsetY"].is_null() && !json["PositionOffsetZ"].is_null())
+	{
+		const auto x = json["PositionOffsetX"].get<float>();
+		const auto y = json["PositionOffsetY"].get<float>();
+		const auto z = json["PositionOffsetZ"].get<float>();
+
+		component.PositionOffsetVector = DirectX::XMFLOAT3(x, y, z);
+	}
+
+	if (!json["RotationOffsetX"].is_null() && !json["RotationOffsetY"].is_null() && !json["RotationOffsetZ"].is_null())
+	{
+		auto x = json["RotationOffsetX"].get<float>();
+		auto y = json["RotationOffsetY"].get<float>();
+		auto z = json["RotationOffsetZ"].get<float>();
+
+		x *= M_PI / 180.0f;
+		y *= M_PI / 180.0f;
+		z *= M_PI / 180.0f;
+
+		component.RotationOffsetVector = DirectX::XMFLOAT3(x, y, z);
+	}
 }
