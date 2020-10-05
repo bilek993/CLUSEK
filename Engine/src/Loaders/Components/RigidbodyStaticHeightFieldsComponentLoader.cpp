23579aa1765cfbc7fa0b1a559d8a16117ef02082
bilek993 <jbilinskipl@gmail.com>
2020-10-05 18:50:35 +0200

    Position and rotation offset can be set in static height field component.

--- Engine/src/Loaders/Components/RigidbodyStaticHeightFieldsComponentLoader.cpp
+++ Engine/src/Loaders/Components/RigidbodyStaticHeightFieldsComponentLoader.cpp
@@ -5,4 +5,26 @@ void RigidbodyStaticHeightFieldsComponentLoader::Add(nlohmann::json& json, entt:
 	const entt::registry::entity_type& entity)
 {
 	auto &component = registry.assign<RigidbodyStaticHeightFieldsComponent>(entity);
+
+	if (!json["PositionOffsetX"].is_null() && !json["PositionOffsetY"].is_null() && !json["PositionOffsetZ"].is_null())
+	{
+		const auto x = json["PositionOffsetX"].get<float>();
+		const auto y = json["PositionOffsetY"].get<float>();
+		const auto z = json["PositionOffsetZ"].get<float>();
+
+		component.PositionOffsetVector = DirectX::XMFLOAT3(x, y, z);
+	}
+
+	if (!json["RotationOffsetX"].is_null() && !json["RotationOffsetY"].is_null() && !json["RotationOffsetZ"].is_null())
+	{
+		auto x = json["RotationOffsetX"].get<float>();
+		auto y = json["RotationOffsetY"].get<float>();
+		auto z = json["RotationOffsetZ"].get<float>();
+
+		x *= M_PI / 180.0f;
+		y *= M_PI / 180.0f;
+		z *= M_PI / 180.0f;
+
+		component.RotationOffsetVector = DirectX::XMFLOAT3(x, y, z);
+	}
 }
