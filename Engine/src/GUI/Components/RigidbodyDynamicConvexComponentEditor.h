0007ff893d7ff9f7fab90d99b092d999ca6d7c2f
bilek993 <jbilinskipl@gmail.com>
2020-10-08 12:31:17 +0200

    Added editor for RigidbodyDynamicConvexComponent.

---
+++ Engine/src/GUI/Components/RigidbodyDynamicConvexComponentEditor.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "BaseComponentEditor.h"
+#include "../../ECS/Components/RigidbodyDynamicConvexComponent.h"
+
+class RigidbodyDynamicConvexComponentEditor final : public BaseComponentEditor<RigidbodyDynamicConvexComponent>
+{
+};
