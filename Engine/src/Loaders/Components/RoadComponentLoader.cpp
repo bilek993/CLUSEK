89fc728b90788ad1bf376415ec2bf876abc5ac10
bilek993 <jbilinskipl@gmail.com>
2020-11-03 18:22:16 +0100

    Added loading points.

--- Engine/src/Loaders/Components/RoadComponentLoader.cpp
+++ Engine/src/Loaders/Components/RoadComponentLoader.cpp
@@ -1,9 +1,21 @@
 #include "RoadComponentLoader.h"
-#include "../../ECS/Components/RoadComponent.h"
 
 void RoadComponentLoader::Add(nlohmann::json& json, entt::registry& registry, const entt::registry::entity_type& entity)
 {
 	auto &component = registry.assign<RoadComponent>(entity);
 
-	// TODO: Add implementation
+	LoadPoints(component, json);
+}
+
+void RoadComponentLoader::LoadPoints(RoadComponent& component, nlohmann::json& json) const
+{
+	for (auto& pointJson : json["Points"])
+	{
+		DirectX::XMFLOAT3 point{};
+		point.x = pointJson["PositionX"];
+		point.y = pointJson["PositionY"];
+		point.z = pointJson["PositionZ"];
+		
+		component.Points.emplace_back(point);
+	}
 }
--- Engine/src/Loaders/Components/RoadComponentLoader.h
+++ Engine/src/Loaders/Components/RoadComponentLoader.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "BaseComponentsLoader.h"
+#include "../../ECS/Components/RoadComponent.h"
 
 class RoadComponentLoader final : public BaseComponentsLoader
 {
 public:
 	void Add(nlohmann::json& json, entt::registry& registry, const entt::registry::entity_type &entity) override;
+
+private:
+	void LoadPoints(RoadComponent& component, nlohmann::json& json) const;
 };
