44f746e6dc9c62d64eeb02981dbc3804919498b6
bilek993 <jbilinskipl@gmail.com>
2020-11-07 10:37:53 +0100

    Added selecting and moving road points.

--- Engine/src/GUI/Components/RoadComponentEditor.cpp
+++ Engine/src/GUI/Components/RoadComponentEditor.cpp
@@ -1,20 +1,27 @@
 #include "RoadComponentEditor.h"
 
+#include <ImGuizmo.h>
+
+
 #include "../../Utils/CameraLocator.h"
 #include "../../Utils/GuiTransformUtil.h"
 
 void RoadComponentEditor::Draw()
 {
 	const auto componentPointer = GetPointerToThisComponent();
+
+	const auto mainCamera = CameraLocator::GetMainCamera(Registry);
+	const auto viewProjectionMatrix = mainCamera.ViewMatrix * mainCamera.ProjectionMatrix;
 	
 	DrawPoints(componentPointer);
 	ImGui::Separator();
 	DrawControlButtons(componentPointer);
 
-	DrawPointsWithGizmosOnScreen(componentPointer);
+	DrawPoints(componentPointer, viewProjectionMatrix);
+	DrawGizmos(componentPointer, mainCamera.ViewMatrix, mainCamera.ProjectionMatrix);
 }
 
-void RoadComponentEditor::DrawPoints(RoadComponent* componentPointer) const
+void RoadComponentEditor::DrawPoints(RoadComponent* componentPointer)
 {
 	auto counter = 0;
 
@@ -22,11 +29,17 @@ void RoadComponentEditor::DrawPoints(RoadComponent* componentPointer) const
 	
 	for (auto& point : componentPointer->Points)
 	{
-		if (ImGui::TreeNode(("Point " + std::to_string(counter++)).c_str()))
+		if (ImGui::TreeNode(("Point " + std::to_string(counter)).c_str()))
 		{
 			ImGui::InputFloat3("Position", &point.x, 3);
+
+			if (ImGui::Button("Select"))
+				SelectedPointId = counter;
+			
 			ImGui::TreePop();
 		}
+
+		counter++;
 	}
 }
 
@@ -52,12 +65,12 @@ void RoadComponentEditor::DrawControlButtons(RoadComponent* componentPointer) co
 	}
 }
 
-void RoadComponentEditor::DrawPointsWithGizmosOnScreen(RoadComponent* componentPointer) const
+void RoadComponentEditor::DrawPoints(RoadComponent* componentPointer, const DirectX::XMMATRIX& viewProjectionMatrix)
 {
 	const auto& io = ImGui::GetIO();
-	
-	const auto mainCamera = CameraLocator::GetMainCamera(Registry);
-	const auto viewProjectionMatrix = mainCamera.ViewMatrix * mainCamera.ProjectionMatrix;
+
+
+	auto counter = 0;
 	
 	for (auto& point : componentPointer->Points)
 	{
@@ -72,6 +85,44 @@ void RoadComponentEditor::DrawPointsWithGizmosOnScreen(RoadComponent* componentP
 
 		const auto isCursorTouchingPoint = GuiTransformUtil::IsTouching(circlePosition, io.MousePos, DOT_SIZE);
 		
+		if (isCursorTouchingPoint && ImGui::IsMouseClicked(0) && !ImGui::IsAnyItemHovered() && !ImGui::IsAnyItemActive())
+			SelectedPointId = counter;
+		
 		FullscreenDrawList->AddCircleFilled(circlePosition, DOT_SIZE, isCursorTouchingPoint ? DOT_COLOR_SECONDARY : DOT_COLOR_PRIMARY);
+		
+		counter++;
 	}
+}
+
+void RoadComponentEditor::DrawGizmos(RoadComponent* componentPointer, const DirectX::XMMATRIX& viewMatrix, const DirectX::XMMATRIX& projectionMatrix) const
+{
+	if (SelectedPointId < 0 || SelectedPointId >= componentPointer->Points.size())
+		return;
+
+	auto worldMatrix = DirectX::XMMatrixTranslation(componentPointer->Points[SelectedPointId].x, 
+													componentPointer->Points[SelectedPointId].y, 
+													componentPointer->Points[SelectedPointId].z);
+
+	DirectX::XMFLOAT4X4 viewMatrixFloats{};
+	XMStoreFloat4x4(&viewMatrixFloats, viewMatrix);
+
+	DirectX::XMFLOAT4X4 projectionMatrixFloats{};
+	XMStoreFloat4x4(&projectionMatrixFloats, projectionMatrix);
+
+	DirectX::XMFLOAT4X4 worldMatrixFloats{};
+	XMStoreFloat4x4(&worldMatrixFloats, worldMatrix);
+
+	auto& io = ImGui::GetIO();
+	ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+	Manipulate(&viewMatrixFloats._11, &projectionMatrixFloats._11, ImGuizmo::TRANSLATE, ImGuizmo::WORLD, &worldMatrixFloats._11);
+
+	worldMatrix = XMLoadFloat4x4(&worldMatrixFloats);
+
+	DirectX::XMVECTOR newScaleVector{};
+	DirectX::XMVECTOR newRotationVector{};
+	DirectX::XMVECTOR newTranslationVector{};
+	
+	XMMatrixDecompose(&newScaleVector, &newRotationVector, &newTranslationVector, worldMatrix);
+
+	XMStoreFloat3(&componentPointer->Points[SelectedPointId], newTranslationVector);
 }
--- Engine/src/GUI/Components/RoadComponentEditor.h
+++ Engine/src/GUI/Components/RoadComponentEditor.h
@@ -8,10 +8,13 @@ protected:
 	void Draw() override;
 
 private:
-	void DrawPoints(RoadComponent* componentPointer) const;
+	void DrawPoints(RoadComponent* componentPointer);
 	void DrawControlButtons(RoadComponent* componentPointer) const;
 
-	void DrawPointsWithGizmosOnScreen(RoadComponent* componentPointer) const;
+	void DrawPoints(RoadComponent* componentPointer, const DirectX::XMMATRIX& viewProjectionMatrix);
+	void DrawGizmos(RoadComponent* componentPointer, const DirectX::XMMATRIX& viewMatrix, const DirectX::XMMATRIX& projectionMatrix) const;
+
+	int SelectedPointId = -1;
 
 	inline const static float DOT_SIZE = 7.0f;
 	inline const static ImColor DOT_COLOR_PRIMARY = ImColor(255, 255, 255);
