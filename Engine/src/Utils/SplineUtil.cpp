6e057d4b467cd2bcb262f135af9d89f11c1171d0
bilek993 <jbilinskipl@gmail.com>
2020-11-07 15:03:54 +0100

    Revert "Removed redundant functions from SplineUtil."
    This reverts commit d40d3eadfc72388e75af5a1df94371796ca2cdc4.

--- Engine/src/Utils/SplineUtil.cpp
+++ Engine/src/Utils/SplineUtil.cpp
@@ -8,10 +8,91 @@ DirectX::XMVECTOR SplineUtil::CalculateBezierQuadraticCurve(const DirectX::XMVEC
 	return DirectX::XMVectorLerp(p0, p1, t);;
 }
 
+DirectX::XMFLOAT4 SplineUtil::CalculateBezierQuadraticCurve(const DirectX::XMFLOAT4& a, const DirectX::XMFLOAT4& b,
+	const DirectX::XMFLOAT4& c, const float t)
+{
+	const auto aVector = XMLoadFloat4(&a);
+	const auto bVector = XMLoadFloat4(&b);
+	const auto cVector = XMLoadFloat4(&c);
+
+	DirectX::XMFLOAT4 result{};
+	XMStoreFloat4(&result, CalculateBezierQuadraticCurve(aVector, bVector, cVector, t));
+
+	return result;
+}
+
+DirectX::XMFLOAT3 SplineUtil::CalculateBezierQuadraticCurve(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b,
+	const DirectX::XMFLOAT3& c, const float t)
+{
+	const auto aVector = XMLoadFloat3(&a);
+	const auto bVector = XMLoadFloat3(&b);
+	const auto cVector = XMLoadFloat3(&c);
+
+	DirectX::XMFLOAT3 result{};
+	XMStoreFloat3(&result, CalculateBezierQuadraticCurve(aVector, bVector, cVector, t));
+
+	return result;
+}
+
+DirectX::XMFLOAT2 SplineUtil::CalculateBezierQuadraticCurve(const DirectX::XMFLOAT2& a, const DirectX::XMFLOAT2& b,
+	const DirectX::XMFLOAT2& c, const float t)
+{
+	const auto aVector = XMLoadFloat2(&a);
+	const auto bVector = XMLoadFloat2(&b);
+	const auto cVector = XMLoadFloat2(&c);
+
+	DirectX::XMFLOAT2 result{};
+	XMStoreFloat2(&result, CalculateBezierQuadraticCurve(aVector, bVector, cVector, t));
+
+	return result;
+}
+
 DirectX::XMVECTOR SplineUtil::CalculateBezierCubicCurve(const DirectX::XMVECTOR& a, const DirectX::XMVECTOR& b,
 	const DirectX::XMVECTOR& c, const DirectX::XMVECTOR& d, const float t)
 {
 	const auto p0 = CalculateBezierQuadraticCurve(a, b, c, t);
 	const auto p1 = CalculateBezierQuadraticCurve(b, c, d, t);
 	return DirectX::XMVectorLerp(p0, p1, t);
+}
+
+DirectX::XMFLOAT4 SplineUtil::CalculateBezierCubicCurve(const DirectX::XMFLOAT4& a, const DirectX::XMFLOAT4& b,
+	const DirectX::XMFLOAT4& c, const DirectX::XMFLOAT4& d, const float t)
+{
+	const auto aVector = XMLoadFloat4(&a);
+	const auto bVector = XMLoadFloat4(&b);
+	const auto cVector = XMLoadFloat4(&c);
+	const auto dVector = XMLoadFloat4(&d);
+
+	DirectX::XMFLOAT4 result{};
+	XMStoreFloat4(&result, CalculateBezierCubicCurve(aVector, bVector, cVector, dVector, t));
+
+	return result;
+}
+
+DirectX::XMFLOAT3 SplineUtil::CalculateBezierCubicCurve(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b,
+	const DirectX::XMFLOAT3& c, const DirectX::XMFLOAT3& d, const float t)
+{
+	const auto aVector = XMLoadFloat3(&a);
+	const auto bVector = XMLoadFloat3(&b);
+	const auto cVector = XMLoadFloat3(&c);
+	const auto dVector = XMLoadFloat3(&d);
+
+	DirectX::XMFLOAT3 result{};
+	XMStoreFloat3(&result, CalculateBezierCubicCurve(aVector, bVector, cVector, dVector, t));
+
+	return result;
+}
+
+DirectX::XMFLOAT2 SplineUtil::CalculateBezierCubicCurve(const DirectX::XMFLOAT2& a, const DirectX::XMFLOAT2& b,
+	const DirectX::XMFLOAT2& c, const DirectX::XMFLOAT2& d, const float t)
+{
+	const auto aVector = XMLoadFloat2(&a);
+	const auto bVector = XMLoadFloat2(&b);
+	const auto cVector = XMLoadFloat2(&c);
+	const auto dVector = XMLoadFloat2(&d);
+
+	DirectX::XMFLOAT2 result{};
+	XMStoreFloat2(&result, CalculateBezierCubicCurve(aVector, bVector, cVector, dVector, t));
+
+	return result;
 }
