ac38e0e07107821f058601d7ecbbf976945100e2
bilek993 <jbilinskipl@gmail.com>
2020-12-30 19:37:32 +0100

    Added better error handling in remapper.

--- Engine/src/Utils/MathUtil.cpp
+++ Engine/src/Utils/MathUtil.cpp
@@ -5,11 +5,10 @@ float MathUtil::Remap(const float value, const float start1, const float stop1, 
 {
 	const auto outgoing = start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
 
-	if (isnan(outgoing) || isinf(outgoing))
-	{
-		Logger::Error("Remapping failed!");
-		return value;
-	}
+	if (isnan(outgoing))
+		Logger::Error("Result of remapping is not a number!");
+	else if (isinf(outgoing))
+		Logger::Error("Result of remapping is infinity or -infinity!");
 
 	return outgoing;
 }
